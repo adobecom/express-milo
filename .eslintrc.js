1247fde93bf4c67f38951f6eca7795e0c62be5bf
David Catalan <40354404+catalan-adobe@users.noreply.github.com>
2023-09-26 15:41:42 +0200

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  root: true,
+  extends: 'airbnb-base',
+  env: { browser: true, mocha: true },
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    allowImportExportEverywhere: true,
+    sourceType: 'module',
+    requireConfigFile: false,
+  },
+  rules: {
+    'no-param-reassign': [2, { props: false }],
+    'linebreak-style': ['error', 'unix'],
+    'import/extensions': ['error', { js: 'always' }],
+    'object-curly-newline': ['error', {
+      ObjectExpression: { multiline: true, minProperties: 6 },
+      ObjectPattern: { multiline: true, minProperties: 6 },
+      ImportDeclaration: { multiline: true, minProperties: 6 },
+      ExportDeclaration: { multiline: true, minProperties: 6 },
+    }],
+    'no-return-assign': ['error', 'except-parens'],
+    'no-unused-expressions': 0,
+    'chai-friendly/no-unused-expressions': 2,
+  },
+  overrides: [
+    {
+      files: ['test/**/*.js'],
+      rules: { 'no-console': 'off' },
+    },
+  ],
+  plugins: [
+    'chai-friendly',
+  ],
+};
