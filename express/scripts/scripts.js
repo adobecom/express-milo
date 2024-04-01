9f061bc206328fb5e47019945f2293663573c2c0
Eric S Chen <all42238@adobe.com>
2024-04-01 13:36:01 -0700

    Update scripts.js

--- express/scripts/scripts.js
+++ express/scripts/scripts.js
@@ -11,7 +11,7 @@
  */
 
 import { setLibs, decorateArea, listenMiloEvents } from './utils.js';
-import {autoUpdateContent} from "./content-replace.js"
+import { autoUpdateContent } from "./content-replace.js"
 
 // Add project-wide style path here.
 const STYLES = ['/express/styles/styles.css'];
@@ -109,8 +109,8 @@ const miloLibs = setLibs(LIBS);
   } else if (getMetadata('breadcrumbs') === 'on' && !!getMetadata('breadcrumbs-base') && (!!getMetadata('short-title') || !!getMetadata('breadcrumbs-page-title'))) document.body.classList.add('breadcrumbs-spacing');
 
   loadLana({ clientId: 'express' });
-  console.log(config)
- 
+  console.log(config);
+
   const isMobileGating = ['yes', 'true', 'on'].includes(getMetadata('mobile-benchmark')?.toLowerCase()) && document.body.dataset.device === 'mobile';
   const rushGating = ['yes', 'on', 'true'].includes(getMetadata('rush-beta-gating')?.toLowerCase());
   const runGating = () => {
@@ -129,8 +129,10 @@ const miloLibs = setLibs(LIBS);
   document.head.append(footerMeta);
 
   listenMiloEvents();
+  if (getMetadata('sheet-powered') === 'Y') {
+    autoUpdateContent(document.getElementsByTagName('main')[0], miloLibs)
+  }
 
-  autoUpdateContent( document.getElementsByTagName('main')[0], miloLibs)
   await loadArea();
 
   if (isMobileGating && rushGating) { runGating(); }
