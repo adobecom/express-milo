125e81abd53e879a6c98e8ba424c61f6fe2b840c
Eric S Chen <all42238@adobe.com>
2024-04-03 13:44:31 -0700

    init

---
+++ express/blocks/seo-nav/seo-nav.js
@@ -0,0 +1,54 @@
+import buildCarousel from '../../scripts/widgets/carousel.js';
+import { addTempWrapperDeprecated } from '../../scripts/utils/decorate.js';
+
+function decorateCarousel(links, container) {
+  links.forEach((p) => {
+    const link = p.querySelector('a');
+    link.classList.add('small', 'secondary', 'fill');
+    link.classList.remove('accent');
+  });
+  buildCarousel('p.button-container', container);
+}
+
+export function updatePillsByCKG(block, carouselDiv) {
+  return (mutationList, observer) => {
+    for (const mutation of mutationList) {
+      if (mutation.type === 'childList') {
+        if (carouselDiv.querySelector('.carousel-container')) {
+          observer.disconnect();
+          return;
+        }
+
+        const newLinks = [...block.querySelectorAll('p.button-container')];
+        if (!newLinks.length) {
+          carouselDiv.style.display = 'none';
+        }
+        decorateCarousel(newLinks, carouselDiv);
+        observer.disconnect();
+        return;
+      }
+    }
+  };
+}
+
+export default function decorate(block) {
+  addTempWrapperDeprecated(block, 'seo-nav');
+
+  const links = [...block.querySelectorAll('p.button-container')];
+  const seoCopy = block.querySelectorAll('div')[block.querySelectorAll('div').length - 1];
+  const carouselDiv = block.querySelector('div:nth-of-type(2) > div');
+
+  if (links.length) {
+    decorateCarousel(links, carouselDiv);
+  }
+
+  if (seoCopy) {
+    const $paragraphs = seoCopy.querySelectorAll('p');
+    for (let i = 0; i < $paragraphs.length; i += 1) {
+      $paragraphs[i].classList.add('seo-paragraph');
+    }
+  }
+
+  const observer = new MutationObserver(updatePillsByCKG(block, carouselDiv));
+  observer.observe(carouselDiv, { childList: true });
+}
