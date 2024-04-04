d27e27127f5786327caccaba1e55294a8d0f3c0a
Eric S Chen <all42238@adobe.com>
2024-04-04 13:29:46 -0700

    initial

---
+++ express/blocks/long-text/long-text.js
@@ -0,0 +1,19 @@
+import { addTempWrapperDeprecated } from '../../scripts/utils/decorate.js';
+
+export default function decorate(block) {
+  if (!block.parentElement.classList.contains('long-text-wrapper')) {
+    addTempWrapperDeprecated(block, 'long-text');
+  }
+
+  if (block.classList.contains('plain')) {
+    block.parentElement.classList.add('plain');
+  }
+
+  if (block.textContent.trim() === '') {
+    if (block.parentElement.classList.contains('long-text-wrapper')) {
+      block.parentElement.remove();
+    } else {
+      block.remove();
+    }
+  }
+}
