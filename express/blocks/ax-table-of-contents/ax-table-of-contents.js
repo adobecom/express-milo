e4cc6a9ec3916ab34ca481d2dc601ea6cab01de9
Robert <all66020@adobe.com>
2024-04-16 04:57:36 -0700

    refactor ax-table-of-contents

--- express/blocks/ax-table-of-contents/ax-table-of-contents.js
+++ express/blocks/ax-table-of-contents/ax-table-of-contents.js
@@ -4,24 +4,25 @@ import { addTempWrapperDeprecated } from '../../scripts/utils/decorate.js';
 
 const { createTag } = await import(`${getLibs()}/utils/utils.js`);
 
-export default function decorate($block, name, doc) {
-  addTempWrapperDeprecated($block, 'ax-table-of-contents');
+// eslint-disable-next-line no-unused-vars
+export default function decorate(block) {
+  addTempWrapperDeprecated(block, 'ax-table-of-contents');
 
-  const config = readBlockConfig($block);
-  const $headings = doc.querySelectorAll('main h2, main h3, main h4, main .ax-table-of-contents');
+  const config = readBlockConfig(block);
+  const headings = document.body.querySelectorAll('main h2, main h3, main h4, main .ax-table-of-contents');
   let skip = true;
-  const $toc = createTag('div', { class: 'toc' });
-  $headings.forEach(($h) => {
-    if (!skip && $h.tagName.startsWith('H')) {
-      const hLevel = +$h.tagName.substring(1);
+  const toc = createTag('div', { class: 'toc' });
+  headings.forEach((h) => {
+    if (!skip && h.tagName.startsWith('H')) {
+      const hLevel = +h.tagName.substring(1);
       if (hLevel <= +config.levels + 1) {
-        const $entry = createTag('div', { class: `toc-entry toc-level-h${hLevel}` });
-        $entry.innerHTML = `<a href="#${$h.id}">${$h.innerHTML}</a>`;
-        $toc.appendChild($entry);
+        const entry = createTag('div', { class: `toc-entry toc-level-h${hLevel}` });
+        entry.innerHTML = `<a href="#${h.id}">${h.innerHTML}</a>`;
+        toc.appendChild(entry);
       }
     }
-    if ($h === $block) skip = false;
+    if (h === block) skip = false;
   });
-  $block.innerHTML = '';
-  $block.appendChild($toc);
+  block.innerHTML = '';
+  block.appendChild(toc);
 }
