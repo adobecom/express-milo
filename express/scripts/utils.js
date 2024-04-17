4414262b264f08b90f9c7e34fb09d600b36f31fb
Qiyun Dai <pianoquin@gmail.com>
2024-04-17 06:38:32 -0500

    Marquee Transpilation (#9)
    * mostly working with further component fixes needed
    
    * remove console.log
    
    * transpile a variant class
    
    * more native buttons approach with sub-text support
    
    * marquee variant styling correction
    
    * no color detection
    
    * Update styles-deprecated.css
    
    * CSS split
    
    * fixed video play link look
    
    * lint
    
    * add double icon generation guard
    
    * make transpilation syncronous
    
    ---------
    
    Co-authored-by: Victor Hargrave <vhargrave@adobe.com>

--- express/scripts/utils.js
+++ express/scripts/utils.js
@@ -29,12 +29,6 @@ export const [setLibs, getLibs] = (() => {
   ];
 })();
 
-export function toClassName(name) {
-  return name && typeof name === 'string'
-    ? name.toLowerCase().replace(/[^0-9a-z]/gi, '-')
-    : '';
-}
-
 /*
  * ------------------------------------------------------------
  * Edit above at your own risk.
@@ -42,6 +36,35 @@ export function toClassName(name) {
  * Note: This file should have no self-invoking functions.
  * ------------------------------------------------------------
  */
+
+function createTag(tag, attributes, html, options = {}) {
+  const el = document.createElement(tag);
+  if (html) {
+    if (html instanceof HTMLElement
+        || html instanceof SVGElement
+        || html instanceof DocumentFragment) {
+      el.append(html);
+    } else if (Array.isArray(html)) {
+      el.append(...html);
+    } else {
+      el.insertAdjacentHTML('beforeend', html);
+    }
+  }
+  if (attributes) {
+    Object.entries(attributes).forEach(([key, val]) => {
+      el.setAttribute(key, val);
+    });
+  }
+  options.parent?.append(el);
+  return el;
+}
+
+export function toClassName(name) {
+  return name && typeof name === 'string'
+    ? name.toLowerCase().replace(/[^0-9a-z]/gi, '-')
+    : '';
+}
+
 export function readBlockConfig(block) {
   const config = {};
   block.querySelectorAll(':scope>div').forEach(($row) => {
@@ -75,6 +98,7 @@ export function readBlockConfig(block) {
 
 export function removeIrrelevantSections(area) {
   if (!area) return;
+
   const getMetadata = (name, doc = document) => {
     const attr = name && name.includes(':') ? 'property' : 'name';
     const meta = doc.head.querySelector(`meta[${attr}="${name}"]`);
@@ -214,12 +238,141 @@ export function listenMiloEvents() {
   window.addEventListener('milo:postSection:loading', postSectionLoadingHandler);
 }
 
+function transpileMarquee(area) {
+  const handleSubCTAText = (oldContainer, newContainer) => {
+    const elAfterBtn = oldContainer.nextElementSibling;
+    if (!elAfterBtn || elAfterBtn?.tagName !== 'BLOCKQUOTE') return;
+
+    const subText = elAfterBtn.querySelector('p');
+
+    if (subText) {
+      const subTextEl = createTag('span', { class: 'cta-sub-text' }, subText.innerHTML);
+      newContainer.append(subTextEl);
+    }
+    elAfterBtn.remove();
+  };
+
+  const needsTranspile = (block) => {
+    const firstRow = block.querySelector(':scope > div:first-of-type');
+    return firstRow.children.length > 1 && ['default', 'mobile', 'desktop', 'hd'].includes(firstRow.querySelector(':scope > div')?.textContent?.trim().toLowerCase());
+  };
+
+  const isVideoLink = (url) => {
+    if (!url) return null;
+    return url.includes('youtube.com/watch')
+      || url.includes('youtu.be/')
+      || url.includes('vimeo')
+      || /.*\/media_.*(mp4|webm|m3u8)$/.test(new URL(url).pathname);
+  };
+
+  const transpile = (block) => {
+    const assetArea = createTag('div');
+
+    block.classList.add('transpiled', 'xl-button');
+
+    if (block.classList.contains('short')) {
+      block.classList.remove('short');
+      block.classList.add('small');
+    }
+
+    if (!block.classList.contains('dark')) {
+      block.classList.add('light');
+    }
+
+    const rows = block.querySelectorAll(':scope > div');
+
+    if (rows.length) {
+      rows.forEach((r, i, arr) => {
+        if (i < arr.length - 1) {
+          r.querySelector(':scope > div:first-of-type')?.remove();
+
+          if (document.body.dataset.device === 'mobile') {
+            const valCol = r.querySelector(':scope > div:last-of-type');
+            assetArea.innerHTML = valCol.innerHTML;
+            if (block.classList.contains('dark')) valCol.innerHTML = '#000000';
+            if (block.classList.contains('light')) valCol.innerHTML = '#ffffff00';
+          }
+
+          if (i > 0) {
+            r.remove();
+          }
+        }
+
+        if (i === arr.length - 1) {
+          const aTags = r.querySelectorAll('p > a');
+          const btnContainers = [];
+          const elsToAppend = [];
+          const actionArea = createTag('p', { class: 'action-area' });
+
+          aTags.forEach((a) => {
+            if (!btnContainers.includes(a.parentElement)) {
+              btnContainers.push(a.parentElement);
+            }
+
+            if (isVideoLink(a.href) && !a.querySelector('span.icon.icon-play')) {
+              const playIcon = createTag('span', { class: 'icon icon-play' });
+              a.prepend(playIcon);
+            }
+          });
+
+          const isInlineButtons = btnContainers.length === 1;
+
+          aTags.forEach((a) => {
+            const buttonContainer = a.parentElement;
+
+            if (buttonContainer?.childNodes.length === 1) {
+              const buttonWrapper = createTag('span');
+              buttonWrapper.append(a);
+              handleSubCTAText(buttonContainer, buttonWrapper);
+              buttonContainer.remove();
+
+              elsToAppend.push(buttonWrapper);
+            }
+          });
+
+          elsToAppend.forEach((e, index) => {
+            actionArea.append(e);
+            const link = e.querySelector('a');
+
+            if (!link) return;
+            if (index === 0) {
+              const strong = createTag('strong');
+              e.prepend(strong);
+              strong.append(link);
+            }
+            if (index === 1) {
+              if (!isInlineButtons) {
+                const em = createTag('em');
+                e.prepend(em);
+                em.append(link);
+              }
+            }
+          });
+
+          const lastPInFirstDiv = r.querySelector(':scope > div > p:last-of-type');
+          lastPInFirstDiv?.after(actionArea);
+          r.append(assetArea);
+        }
+      });
+    }
+  };
+
+  const marquees = [...area.querySelectorAll('div.marquee')].filter((m) => m.classList[0] === 'marquee');
+  marquees.forEach((block) => {
+    if (needsTranspile(block)) transpile(block);
+  });
+}
+
 export function decorateArea(area = document) {
-  removeIrrelevantSections(area);
+  document.body.dataset.device = navigator.userAgent.includes('Mobile') ? 'mobile' : 'desktop';
+  removeIrrelevantSections(area.tagName === 'main' ? area : area.querySelector('main', 'body'));
   // LCP image decoration
   (function decorateLCPImage() {
     const lcpImg = area.querySelector('img');
     lcpImg?.removeAttribute('loading');
   }());
+
+  // transpile conflicting blocks
+  transpileMarquee(area);
   overrideMiloColumns(area);
 }
