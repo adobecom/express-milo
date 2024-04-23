e5dfdac57f5714bc087b159985b683ed66ffcd36
Robert <all66020@adobe.com>
2024-04-23 12:03:51 -0700

    import test for blog-posts

---
+++ test/blocks/blog-posts/blog-posts.test.js
@@ -0,0 +1,6 @@
+/* eslint-env mocha */
+/* eslint-disable no-unused-vars */
+
+await import('../../../express/scripts/scripts.js');
+
+const { default: decorate } = await import('../../../express/blocks/blog-posts/blog-posts.js');
