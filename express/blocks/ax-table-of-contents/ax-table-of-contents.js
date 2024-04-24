a63d1011348956a2afe6231a9b3e84b5ab7096c6
Robert Lopez <159060951+rl-adobe@users.noreply.github.com>
2024-04-24 07:57:28 -0700

    MWPW-143405: table-of-contents block (#20)
    * move table-of-contents block
    
    * replace table-of-contents to ax-table-of-contents
    
    * refactor ax-table-of-contents
    
    * update utils.js not to replace milo toc
    
    * set margin to toc
    
    * set variant max-width to the toc
    
    * import test for table-of-contents
    
    * fix typo
    
    ---------
    
    Co-authored-by: Victor Hargrave <vhargrave@adobe.com>

---
+++ express/blocks/ax-table-of-contents/ax-table-of-contents.js
@@ -0,0 +1,28 @@
+/* eslint-disable import/named, import/extensions */
+import { getLibs, readBlockConfig } from '../../scripts/utils.js';
+import { addTempWrapperDeprecated } from '../../scripts/utils/decorate.js';
+
+const { createTag } = await import(`${getLibs()}/utils/utils.js`);
+
+// eslint-disable-next-line no-unused-vars
+export default function decorate(block) {
+  addTempWrapperDeprecated(block, 'ax-table-of-contents');
+
+  const config = readBlockConfig(block);
+  const headings = document.body.querySelectorAll('main h2, main h3, main h4, main .ax-table-of-contents');
+  let skip = true;
+  const toc = createTag('div', { class: 'toc' });
+  headings.forEach((h) => {
+    if (!skip && h.tagName.startsWith('H')) {
+      const hLevel = +h.tagName.substring(1);
+      if (hLevel <= +config.levels + 1) {
+        const entry = createTag('div', { class: `toc-entry toc-level-h${hLevel}` });
+        entry.innerHTML = `<a href="#${h.id}">${h.innerHTML}</a>`;
+        toc.appendChild(entry);
+      }
+    }
+    if (h === block) skip = false;
+  });
+  block.innerHTML = '';
+  block.appendChild(toc);
+}
