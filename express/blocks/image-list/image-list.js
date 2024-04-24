e3095d043f924c9f5c26ab1edee42b73b2bf4342
Robert <all66020@adobe.com>
2024-04-24 05:39:35 -0700

    remove dollar sign from image-list.js

--- express/blocks/image-list/image-list.js
+++ express/blocks/image-list/image-list.js
@@ -2,23 +2,23 @@
 import { toClassName } from '../../scripts/utils.js';
 import { decorateButtonsDeprecated } from '../../scripts/utils/decorate.js';
 
-export default function decorate($block) {
-  decorateButtonsDeprecated($block);
-  const $rows = Array.from($block.children);
-  $rows.forEach(($row) => {
-    const $cells = Array.from($row.children);
-    if ($cells[2]) {
-      const device = toClassName($cells[2].textContent.trim());
-      if (device) $row.classList.add(`${device}-only`);
-      $cells[2].remove();
+export default function decorate(block) {
+  decorateButtonsDeprecated(block);
+  const rows = Array.from(block.children);
+  rows.forEach((row) => {
+    const cells = Array.from(row.children);
+    if (cells[2]) {
+      const device = toClassName(cells[2].textContent.trim());
+      if (device) row.classList.add(`${device}-only`);
+      cells[2].remove();
     }
-    if ($cells[1]) {
-      const $a = $cells[1].querySelector('a');
+    if (cells[1]) {
+      const $a = cells[1].querySelector('a');
       if ($a) {
-        $a.innerHTML = $cells[0].innerHTML;
-        $cells[0].remove();
+        $a.innerHTML = cells[0].innerHTML;
+        cells[0].remove();
       } else {
-        $cells[1].remove();
+        cells[1].remove();
       }
     }
   });
