f193510e2fb76ed4dfde7e1157f4c6988ac3cc3a
Justin Rings <all53703@Justins-MacBook-Pro.local>
2024-08-07 07:28:59 -0700

    move getMobileOperatingSystem out of utils

--- express/scripts/utils.js
+++ express/scripts/utils.js
@@ -494,22 +494,3 @@ export function decorateArea(area = document) {
   overrideMiloColumns(area);
   addJapaneseSectionHeaderSizing(area);
 }
-
-export function getMobileOperatingSystem() {
-  const userAgent = navigator.userAgent || navigator.vendor || window.opera;
-
-  // Windows Phone must come first because its UA also contains "Android"
-  if (/windows phone/i.test(userAgent)) {
-    return 'Windows Phone';
-  }
-
-  if (/android/i.test(userAgent)) {
-    return 'Android';
-  }
-
-  if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-    return 'iOS';
-  }
-
-  return 'unknown';
-}
