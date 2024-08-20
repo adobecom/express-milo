428d4fa219b1fdd60bebddcc148579f5322f8932
Justin Rings <all53703@Justins-MacBook-Pro.local>
2024-08-20 06:11:33 -0700

    feat: add cards block

---
+++ express/blocks/cards/cards.js
@@ -0,0 +1,27 @@
+// eslint-disable-next-line import/no-unresolved
+import { getLibs } from '../../scripts/utils.js';
+
+const { createTag } = await import(`${getLibs()}/utils/utils.js`);
+/**
+ * @param {HTMLDivElement} $block
+ */
+export default function decorate($block) {
+  $block.querySelectorAll(':scope>div').forEach(($card) => {
+    $card.classList.add('card');
+    const $cardDivs = [...$card.children];
+    $cardDivs.forEach(($div) => {
+      if ($div.querySelector('img')) {
+        $div.classList.add('card-image');
+      } else {
+        $div.classList.add('card-content');
+      }
+      const $a = $div.querySelector('a');
+      if ($a && $a.textContent.trim().startsWith('https://')) {
+        const $wrapper = createTag('a', { href: $a.href, class: 'card' });
+        $a.remove();
+        $wrapper.innerHTML = $card.innerHTML;
+        $block.replaceChild($wrapper, $card);
+      }
+    });
+  });
+}
