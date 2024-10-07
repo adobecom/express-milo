1644e9857e712e34b523f62819f35e8780dac152
Justin Rings <all53703@Justins-MacBook-Pro.local>
2024-10-07 13:46:24 -0400

    add sticky-promo-bar block

---
+++ express/blocks/sticky-promo-bar/sticky-promo-bar.js
@@ -0,0 +1,62 @@
+import { getLibs } from '../../scripts/utils.js';
+import BlockMediator from '../../scripts/block-mediator.min.js';
+import { sendEventToAnalytics } from '../../scripts/instrument.js';
+
+const { createTag } = await import(`${getLibs()}/utils/utils.js`);
+
+function initScrollInteraction(block) {
+  const inBodyBanner = block.cloneNode(true);
+  inBodyBanner.dataset.blockStatus = 'loaded';
+  inBodyBanner.classList.add('clone');
+  block.classList.add('inbody');
+  block.after(inBodyBanner);
+
+  const intersectionCallback = (entries) => {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting && inBodyBanner.getBoundingClientRect().top < 0) {
+        block.classList.add('shown');
+      } else {
+        block.classList.remove('shown');
+      }
+    });
+  };
+
+  const observer = new IntersectionObserver(intersectionCallback, {
+    rootMargin: '0px',
+    threshold: 0,
+  });
+
+  observer.observe(inBodyBanner);
+}
+
+export default async function decorate(block) {
+  const close = createTag('button', {
+    class: 'close',
+    'aria-label': 'close',
+  });
+  block.appendChild(close);
+
+  BlockMediator.set('promobar', {
+    block,
+    rendered: true,
+  });
+
+  close.addEventListener('click', () => {
+    block.remove();
+    BlockMediator.set('promobar', {
+      block,
+      rendered: false,
+    });
+    sendEventToAnalytics('adobe.com:express:cta:startYourFreeTrial:close');
+  });
+
+  if (block.classList.contains('loadinbody')) {
+    setTimeout(() => {
+      initScrollInteraction(block);
+    });
+  } else {
+    setTimeout(() => {
+      block.classList.add('shown');
+    }, 10);
+  }
+}
---
+++ test/blocks/sticky-promo-bar/sticky-promo-bar.test.js
@@ -0,0 +1,4 @@
+/* eslint-env mocha */
+/* eslint-disable no-unused-vars */
+
+const { _, default: decorate } = await Promise.all([import('../../../express/scripts/scripts.js'), import('../../../express/blocks/sticky-promo-bar/sticky-promo-bar.js')]);
