9f9f2a25f8d4651ac46fe4d5036d87c5f6513cfb
Kenneth Lum <all00866@adobe.com>
2024-10-31 14:31:39 -0700

    migrating to Milo: responsive - mobile, tablet, desktop (WIP)

--- express/blocks/banner/banner.js
+++ express/blocks/banner/banner.js
@@ -1,24 +1,44 @@
 import { getLibs } from '../../scripts/utils.js';
 import { formatSalesPhoneNumber } from '../../scripts/utils/location-utils.js';
-import { decorateButtonsDeprecated, normalizeHeadings } from '../../scripts/utils/decorate.js';
+import {
+  decorateButtonsDeprecated,
+  normalizeHeadings,
+} from '../../scripts/utils/decorate.js';
 import { fixIcons } from '../../scripts/utils/icons.js';
 
 const { createTag } = await import(`${getLibs()}/utils/utils.js`);
 
 export default async function decorate(block) {
+  // const timeStart = performance.now();
+
   decorateButtonsDeprecated(block);
 
   const isBannerLightVariant = block.classList.contains('light');
   const isBannerStandoutVariant = block.classList.contains('standout');
+  const isBannerCoolVariant = block.classList.contains('cool');
 
   if (isBannerStandoutVariant) {
-    const standoutContainer = createTag('div', { class: 'standout-container' });
+    const contentContainer = createTag('div', {
+      class: 'content-container',
+    });
+    for (const child of block.children) {
+      contentContainer.append(child);
+    }
+    block.replaceChildren(contentContainer);
+  } else if (isBannerCoolVariant) {
+    const topContainer = createTag('div', {
+      class: 'top-container',
+    });
+    const contentContainer = createTag('div', {
+      class: 'content-container',
+    });
+
+    topContainer.append(contentContainer);
 
     for (const child of block.children) {
-      standoutContainer.append(child);
+      contentContainer.append(child);
     }
-
-    block.replaceChildren(standoutContainer);
+    block.replaceChildren(topContainer);
   }
 
   normalizeHeadings(block, ['h2', 'h3']);
@@ -31,7 +51,7 @@ export default async function decorate(block) {
     button.classList.remove('primary');
     button.classList.remove('secondary');
 
-    if (isBannerStandoutVariant) {
+    if (isBannerStandoutVariant || isBannerCoolVariant) {
       button.classList.remove('accent');
       button.classList.add('large', 'primary');
     } else if (isBannerLightVariant) {
@@ -45,9 +65,13 @@ export default async function decorate(block) {
     }
   });
 
-  const phoneNumberTags = block.querySelectorAll('a[title="{{business-sales-numbers}}"]');
+  const phoneNumberTags = block.querySelectorAll(
+    'a[title="{{business-sales-numbers}}"]'
+  );
   if (phoneNumberTags.length > 0) {
     await formatSalesPhoneNumber(phoneNumberTags);
   }
   fixIcons(block);
+
+  // console.log('=== TIME NEEDED', performance.now() - timeStart);
 }
