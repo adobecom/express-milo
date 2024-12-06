ead10f41ca423b165d757744b9ab6395d0a718ae
Victor Hargrave <vhargrave@adobe.com>
2024-12-06 12:12:44 +0100

    DOTCOM-142302 fix pills not appearing template pages

--- express/code/scripts/utils/string.js
+++ express/code/scripts/utils/string.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line import/prefer-default-export
 export function titleCase(str) {
+  if (!str) return '';
   const splitStr = str.toLowerCase().split(' ');
   for (let i = 0; i < splitStr.length; i += 1) {
     splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
