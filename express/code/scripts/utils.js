209ddf91ecfd4deecea4475c536baf6de58c00ff
Victor Hargrave <vhargrave@adobe.com>
2024-12-11 15:06:43 +0100

    hlx 5 upgrade

--- express/code/scripts/utils.js
+++ express/code/scripts/utils.js
@@ -18,10 +18,10 @@ export const [setLibs, getLibs] = (() => {
     (prodLibs, location) => {
       libs = (() => {
         const { hostname, search } = location || window.location;
-        if (!(hostname.includes('.hlx.') || hostname.includes('local'))) return prodLibs;
+        if (!(hostname.includes('.aem.') || hostname.includes('local'))) return prodLibs;
         const branch = new URLSearchParams(search).get('milolibs') || 'main';
         if (branch === 'local') return 'http://localhost:6456/libs';
-        return branch.includes('--') ? `https://${branch}.hlx.live/libs` : `https://${branch}--milo--adobecom.hlx.live/libs`;
+        return branch.includes('--') ? `https://${branch}.aem.live/libs` : `https://${branch}--milo--adobecom.aem.live/libs`;
       })();
       return libs;
     }, () => libs,
@@ -326,6 +326,7 @@ export async function decorateButtonsDeprecated(el, size) {
       if (originalHref !== linkText
           && !(linkText.startsWith('https') && linkText.includes('/media_'))
           && !/hlx\.blob\.core\.windows\.net/.test(linkText)
+          && !/aem\.blob\.core\.windows\.net/.test(linkText)
           && !linkText.endsWith(' >')
           && !(hash === '#embed-video')
           && !linkText.endsWith(' ›')
