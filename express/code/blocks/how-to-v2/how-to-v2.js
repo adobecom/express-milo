11f83b7b59e13216ebc1ae6b1cd02fb6ab3ecbee
Jackson Sandland <all48775@adobe.com>
2025-01-24 13:46:07 -0700

    start removing references to image or video that were authored

--- express/code/blocks/how-to-v2/how-to-v2.js
+++ express/code/blocks/how-to-v2/how-to-v2.js
@@ -1,6 +1,5 @@
 /* eslint-disable import/named, import/extensions */
 import { createTag } from '../../scripts/utils.js';
-import { embedYoutube } from '../../scripts/embed-videos.js';
 
 function setStepDetails(block, indexOpenedStep) {
   const listItems = block.querySelectorAll(':scope li');
@@ -35,7 +34,7 @@ function buildAccordion(block, rows, stepsContent) {
     const listItemContent = createTag('div', { class: 'step-content' });
 
     const detailText = stepDetail;
-    detailText.classList.add('detail-text');
+    detailText && detailText.classList.add('detail-text');
 
     const detailContainer = createTag('div', { class: 'detail-container' });
 
@@ -70,9 +69,6 @@ function buildAccordion(block, rows, stepsContent) {
 }
 
 export default async function decorate(block) {
-  const isVideoVariant = block.classList.contains('video');
-  const isImageVariant = block.classList.contains('image');
-
   const section = block.closest('.section');
   const rows = Array.from(block.children);
 
@@ -85,45 +81,43 @@ export default async function decorate(block) {
     rows.shift();
   }
 
-  if (isVideoVariant || isImageVariant) {
-    const stepsContent = createTag('div', { class: 'steps-content' });
+  const stepsContent = createTag('div', { class: 'steps-content' });
 
-    if (hasBackground) {
-      // So that background image goes beyond container
-      const stepsContentBackground = createTag('div', { class: 'steps-content-backg' });
-      const stepsContentBackgroundImg = createTag('img', { class: 'steps-content-backg-image' });
-      stepsContent.append(stepsContentBackground);
-      stepsContentBackground.append(stepsContentBackgroundImg);
-      stepsContentBackgroundImg.src = backgroundURL;
-    }
-
-    if (isVideoVariant) {
-      const videoData = rows.shift();
-
-      // remove the added social link from the block DOM
-      block.removeChild(block.children[0]);
-
-      const videoLink = videoData.querySelector('a');
-      const youtubeURL = videoLink?.href;
-      const url = new URL(youtubeURL);
-
-      const videoContainerEl = createTag('div', { class: 'video-container' });
-
-      const videoEl = embedYoutube(url);
-      videoEl.classList.add('video-how-to-steps-accordion');
-
-      videoContainerEl.append(videoEl);
-      stepsContent.append(videoContainerEl);
-    } else {
-      const imageData = rows.shift();
-      const imageEl = imageData.querySelector('picture');
-      const imageContainerEl = createTag('div', { class: 'image-container' });
-      imageContainerEl.append(imageEl);
-      stepsContent.append(imageContainerEl);
-    }
-
-    const heading = section.querySelector('h2, h3, h4');
-    block.replaceChildren(heading, stepsContent);
-    buildAccordion(block, rows, stepsContent);
+  if (hasBackground) {
+    // So that background image goes beyond container
+    const stepsContentBackground = createTag('div', { class: 'steps-content-backg' });
+    const stepsContentBackgroundImg = createTag('img', { class: 'steps-content-backg-image' });
+    stepsContent.append(stepsContentBackground);
+    stepsContentBackground.append(stepsContentBackgroundImg);
+    stepsContentBackgroundImg.src = backgroundURL;
   }
+
+  // if (isVideoVariant) {
+  //   const videoData = rows.shift();
+
+  //   // remove the added social link from the block DOM
+  //   block.removeChild(block.children[0]);
+
+  //   const videoLink = videoData.querySelector('a');
+  //   const youtubeURL = videoLink?.href;
+  //   const url = new URL(youtubeURL);
+
+  //   const videoContainerEl = createTag('div', { class: 'video-container' });
+
+  //   const videoEl = embedYoutube(url);
+  //   videoEl.classList.add('video-how-to-steps-accordion');
+
+  //   videoContainerEl.append(videoEl);
+  //   stepsContent.append(videoContainerEl);
+  // } else {
+  //   const imageData = rows.shift();
+  //   const imageEl = imageData.querySelector('picture');
+  //   const imageContainerEl = createTag('div', { class: 'image-container' });
+  //   imageContainerEl.append(imageEl);
+  //   stepsContent.append(imageContainerEl);
+  // }
+
+  const heading = section.querySelector('h2, h3, h4');
+  block.replaceChildren(heading, stepsContent);
+  buildAccordion(block, rows, stepsContent);
 }
