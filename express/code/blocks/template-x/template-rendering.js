1b6aab5096c757472c2ffbddf1af83ae76183ece
echen-adobe <all42238@adobe.com>
2025-02-05 09:51:43 -0800

    implements aria live for the copied to clipboard function

--- express/code/blocks/template-x/template-rendering.js
+++ express/code/blocks/template-x/template-rendering.js
@@ -106,7 +106,7 @@ async function getVideoUrls(renditionLinkHref, componentLinkHref, page) {
   }
 }
 
-async function share(branchUrl, tooltip, timeoutId) {
+async function share(branchUrl, tooltip, timeoutId, liveRegion,text) {
   const urlWithTracking = await getTrackingAppendedURL(branchUrl, {
     placement: 'template-x',
     isSearchOverride: true,
@@ -119,7 +119,8 @@ async function share(branchUrl, tooltip, timeoutId) {
   if (tooltipRightEdgePos > window.innerWidth) {
     tooltip.classList.add('flipped');
   }
-
+  // Update ARIA-live region
+  liveRegion.textContent = text;
   clearTimeout(timeoutId);
   return setTimeout(() => {
     tooltip.classList.remove('display-tooltip');
@@ -138,18 +139,21 @@ function renderShareWrapper(branchUrl) {
     role: 'tooltip',
     tabindex: '-1',
   });
+  const liveRegion = createTag('div', {
+    'aria-live': 'polite',
+    'class': 'sr-only',
+  });
+  wrapper.append(liveRegion);
+
   let timeoutId = null;
-  shareIcon.addEventListener('click', (ev) => {
+  shareIcon.addEventListener('click', async (ev) => {
     ev.preventDefault();
     ev.stopPropagation();
-    timeoutId = share(branchUrl, tooltip, timeoutId);
+    timeoutId = await share(branchUrl, tooltip, timeoutId, liveRegion, text);
   });
-
-  shareIcon.addEventListener('keypress', (e) => {
-    if (e.key !== 'Enter') {
-      return;
-    }
-    timeoutId = share(branchUrl, tooltip, timeoutId);
+  shareIcon.addEventListener('keypress', async (e) => {
+    if (e.key !== 'Enter') return;
+    timeoutId = await share(branchUrl, tooltip, timeoutId, liveRegion, text);
   });
   const checkmarkIcon = getIconElementDeprecated('checkmark-green');
   tooltip.append(checkmarkIcon);
