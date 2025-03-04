f2ab5b0ae2d5021ee75d65765e4f66e0888349c4
Jackson Sandland <all48775@adobe.com>
2025-03-04 14:21:40 -0700

    ensure heaeder is positioned at top and center

--- express/code/blocks/discover-more-cards/discover-more-cards.js
+++ express/code/blocks/discover-more-cards/discover-more-cards.js
@@ -1,5 +1,5 @@
 import { getLibs, addTempWrapperDeprecated } from '../../scripts/utils.js';
-import buildCarousel from '../../scripts/widgets/carousel.js';
+// import buildCarousel from '../../scripts/widgets/carousel.js';
 
 export default async function decorate(block) {
   const { createTag } = await import(`${getLibs()}/utils/utils.js`);
@@ -13,11 +13,18 @@ export default async function decorate(block) {
     rows.shift();
   }
 
+  const header = rows[0]?.querySelector('h1, h2, h3, h4, h5, h6');
+  if (header) {
+    const headerSection = createTag('div', { class: 'discover-more-cards-header' });
+    headerSection.append(header);
+    block.prepend(headerSection);
+    rows.shift();
+  }
+
   rows.forEach((row) => {
     row.classList.add('discover-more-card');
     cardsWrapper.append(row);
   });
 
   block.append(cardsWrapper);
-  await buildCarousel('', block.querySelector('.discover-more-cards-wrapper'));
 }
