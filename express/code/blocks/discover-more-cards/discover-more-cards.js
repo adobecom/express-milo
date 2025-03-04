23c6086f1630d6f388184eab2507a3bd2400d2ad
Jackson Sandland <all48775@adobe.com>
2025-03-04 12:36:37 -0700

    init js and css for discover-more-cards

---
+++ express/code/blocks/discover-more-cards/discover-more-cards.js
@@ -0,0 +1,22 @@
+import { getLibs, addTempWrapperDeprecated } from '../../scripts/utils.js';
+
+export default async function decorate(block) {
+  const { createTag } = await import(`${getLibs()}/utils/utils.js`);
+  addTempWrapperDeprecated(block, 'discover-more-cards');
+
+  const cardsWrapper = createTag('div', { class: 'discover-more-cards-wrapper' });
+  const rows = [...block.children];
+
+  if (rows[0]?.querySelector('img')?.src) {
+    cardsWrapper.style.backgroundImage = `url(${rows[0].querySelector('img').src})`;
+    rows.shift();
+  }
+
+  // Add cards to wrapper
+  rows.forEach((row) => {
+    row.classList.add('discover-more-card');
+    cardsWrapper.append(row);
+  });
+
+  block.append(cardsWrapper);
+}
