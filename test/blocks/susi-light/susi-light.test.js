be58469b0f40cfaab6d55ae2e0904ab96651c647
Jingle Huang <jingleh321@gmail.com>
2025-03-07 13:47:12 -0800

    done ut

--- test/blocks/susi-light/susi-light.test.js
+++ test/blocks/susi-light/susi-light.test.js
@@ -21,10 +21,10 @@ await import(`${getLibs()}/utils/utils.js`).then((mod) => {
 });
 
 describe('Susi-light', async () => {
-  const block = document.querySelector('.susi-light');
+  const blocks = [...document.querySelectorAll('.susi-light')];
   before(async () => {
     window.fetch = sinon.stub().callsFake(() => mockRes({}));
-    await decorate(block);
+    await Promise.all(blocks.map((block) => decorate(block)));
     await delay(310);
   });
 
@@ -33,17 +33,75 @@ describe('Susi-light', async () => {
     window.fetch = originalFetch;
   });
 
-  it('Susi-light gets decorated with required properties', () => {
-    expect(block).to.exist;
-    const component = block.querySelector('susi-sentry-light');
-    expect(!!component.variant).to.be.true;
-    expect(!!component.config).to.be.true;
-    expect(!!component.authParams).to.be.true;
+  it('decorates susi-light with required properties', () => {
+    for (const block of blocks) {
+      expect(block).to.exist;
+      const component = block.querySelector('susi-sentry-light');
+      expect(!!component.variant).to.be.true;
+      expect(!!component.config).to.be.true;
+      expect(!!component.authParams).to.be.true;
+    }
   });
 
-  it('Susi-light gets decorated', () => {
-    const component = block.querySelector('susi-sentry-light');
-    expect(component).to.exist;
-    expect(!!component.shadowRoot).to.be.true;
+  it('loads susi-sentry-light', () => {
+    for (const block of blocks) {
+      const component = block.querySelector('susi-sentry-light');
+      expect(component).to.exist;
+      expect(!!component.shadowRoot).to.be.true;
+    }
+  });
+
+  describe('susi-light tabs variant', () => {
+    const block = document.querySelector('.susi-light.tabs');
+
+    it('allocates content into tabs', () => {
+      expect(block.querySelector('.express-logo')).to.exist;
+      expect(block.querySelector('.title')).to.exist;
+      expect(block.querySelectorAll('[role=tablist] > [role=tab]').length).to.equal(2);
+      expect(block.querySelectorAll('[role=tabpanel]').length).to.equal(2);
+      expect(block.querySelectorAll('[role=tabpanel].standard')).to.exist;
+      expect(block.querySelectorAll('[role=tabpanel].edu-express')).to.exist;
+    });
+
+    it('decorates bubble footer', () => {
+      [...block.querySelectorAll('[role=tabpanel]')].forEach((panel) => {
+        expect(panel.querySelector('.footer')).to.exist;
+      });
+      const bubbleFooter = block.querySelector('.footer:has(h2)');
+      expect(bubbleFooter).to.exist;
+      expect(bubbleFooter.querySelectorAll('.susi-bubble').length).to.equal(2);
+    });
+
+    const [expectTabOneOn, expectTabTwoOn] = [
+      () => {
+        const [tab1, tab2] = [...block.querySelectorAll('[role=tab]')];
+        const [panel1, panel2] = [...block.querySelectorAll('[role=tabpanel]')];
+        expect(tab1.getAttribute('aria-selected')).to.equal('true');
+        expect(tab2.getAttribute('aria-selected')).to.equal('false');
+        expect(panel1.classList.contains('hide')).to.be.false;
+        expect(panel2.classList.contains('hide')).to.be.true;
+      },
+      () => {
+        const [tab1, tab2] = [...block.querySelectorAll('[role=tab]')];
+        const [panel1, panel2] = [...block.querySelectorAll('[role=tabpanel]')];
+        expect(tab1.getAttribute('aria-selected')).to.equal('false');
+        expect(tab2.getAttribute('aria-selected')).to.equal('true');
+        expect(panel1.classList.contains('hide')).to.be.true;
+        expect(panel2.classList.contains('hide')).to.be.false;
+      },
+    ];
+    it('displays first tab by default', () => {
+      expectTabOneOn();
+    });
+    it('switches tabs', () => {
+      const tab1 = block.querySelector('[role=tab]');
+      const tab2 = block.querySelector('[role=tab]:nth-of-type(2)');
+      tab1.click();
+      expectTabOneOn();
+      tab2.click();
+      expectTabTwoOn();
+      tab1.click();
+      expectTabOneOn();
+    });
   });
 });
