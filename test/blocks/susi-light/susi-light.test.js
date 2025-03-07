961a4ee20551a5060a4443d9a46231d78336755f
Jingle Huang <jingleh321@gmail.com>
2025-03-06 16:56:40 -0800

    ut wip

--- test/blocks/susi-light/susi-light.test.js
+++ test/blocks/susi-light/susi-light.test.js
@@ -3,19 +3,34 @@
 
 import { readFile } from '@web/test-runner-commands';
 import { expect } from '@esm-bundle/chai';
+import sinon from 'sinon';
+import { mockRes } from '../test-utilities.js';
+import { delay } from '../../helpers/waitfor.js';
 
-await import('../../../express/code/scripts/scripts.js');
+const originalFetch = window.fetch;
+const locales = { '': { ietf: 'en-US', tk: 'hah7vzn.css' } };
 
 document.body.innerHTML = await readFile({ path: './mocks/body.html' });
+const imports = await Promise.all([import('../../../express/code/scripts/utils.js'), import('../../../express/code/scripts/scripts.js'), import(
+  '../../../express/code/blocks/susi-light/susi-light.js'
+)]);
+const [{ getLibs }, _, { default: decorate }] = imports;
+await import(`${getLibs()}/utils/utils.js`).then((mod) => {
+  const conf = { locales };
+  mod.setConfig(conf);
+});
 
 describe('Susi-light', async () => {
   const block = document.querySelector('.susi-light');
-  const { default: decorate } = await import(
-    '../../../express/code/blocks/susi-light/susi-light.js'
-  );
-  decorate(block);
-  before(() => {
-    window.isTestEnv = true;
+  before(async () => {
+    window.fetch = sinon.stub().callsFake(() => mockRes({}));
+    await decorate(block);
+    await delay(310);
+  });
+
+  after(() => {
+    // Do not build up any test state - reset window.fetch to it's original state
+    window.fetch = originalFetch;
   });
 
   it('Susi-light gets decorated with required properties', () => {
