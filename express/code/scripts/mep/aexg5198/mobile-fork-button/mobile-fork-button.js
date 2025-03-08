54605e17bc39cb613478836aedff472e1d07bceb
AdobeLinhart <dlinhart@adobe.com>
2025-03-07 18:51:14 -0700

    Linting.

--- express/code/scripts/mep/aexg5198/mobile-fork-button/mobile-fork-button.js
+++ express/code/scripts/mep/aexg5198/mobile-fork-button/mobile-fork-button.js
@@ -1,7 +1,7 @@
 import { getLibs, getMobileOperatingSystem, getIconElementDeprecated, addTempWrapperDeprecated } from '../../../utils.js';
 import { createFloatingButton } from '../../../widgets/floating-cta.js';
 
-// import { getLibs, getMobileOperatingSystem, getIconElementDeprecated, addTempWrapperDeprecated } 
+// import { getLibs, getMobileOperatingSystem, getIconElementDeprecated, addTempWrapperDeprecated }
 // from '../../scripts/utils.js';
 // import { createFloatingButton } from '../../scripts/widgets/floating-cta.js';
 
@@ -115,23 +115,29 @@ function collectFloatingButtonData() {
 }
 
 async function mWebStickyCTA() {
-  const blockHTML = `
-    <div class="section" data-status="decorated" data-idx="8">
-      <div class="floating-button-wrapper">
-        <div class="floating-button meta-powered">
-          <div>mobile</div>
-        </div>
-      </div>
-    </div>`;
-  const block = document.querySelector('.mobile-fork-button');
-  block.outerHTML = blockHTML;
+  // const blockHTML = `
+  //   <div class="section" data-status="decorated" data-idx="8">
+  //     <div class="floating-button-wrapper">
+  //       <div class="floating-button meta-powered">
+  //         <div>mobile</div>
+  //       </div>
+  //     </div>
+  //   </div>`;
+  const newBlock = createTag(
+    'div',
+    { class: 'floating-button-wrapper' },
+    '<div class="floating-button meta-powered"><div>mobile</div></div>'
+  );
+  const oldBlock = document.querySelector('.mobile-fork-button');
+  oldBlock.replaceWith(newBlock);
+  // block.outerHTML = blockHTML;
 
   const audience = 'mobile';
   const data = collectFloatingButtonData();
 
-  data.mainCta.text = data.mainCta.forkStickyMobileText ? data.mainCta.forkStickyMobileText : data.mainCta.text;
-  data.mainCta.href = data.mainCta.forkStickyMobileHref ? data.mainCta.forkStickyMobileHref : data.mainCta.href;
-  const buttonWrapper = await createFloatingButton(block, audience, data);
+  data.mainCta.text = data.mainCta.forkStickyMobileText || data.mainCta.text;
+  data.mainCta.href = data.mainCta.forkStickyMobileHref || data.mainCta.href;
+  await createFloatingButton(block, audience, data);
 }
 
 function mWebOverlayScroll() {
@@ -149,7 +155,7 @@ function mWebBuildElements() {
 
   const closeElement = document.createElement('a');
   closeElement.classList.add('mweb-close');
-  closeElement.innerHTML =`
+  closeElement.innerHTML = `
     <svg width="13" height="12" viewBox="0 0 13 12" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M7.45455 5.99986L12.1404 1.31442C12.4041 1.05075 12.4041 0.623598 12.1404 0.359925C11.8767 0.0962522 11.4496 0.0962522 11.1859 0.359925L6.5 5.04537L1.81412 0.359925C1.55044 0.0962522 1.12329 0.0962522 0.85962 0.359925C0.595947 0.623598 0.595947 1.05075 0.85962 1.31442L5.54545 5.99986L0.85962 10.6853C0.595947 10.949 0.595947 11.3761 0.85962 11.6398C0.991452 11.7716 1.16416 11.8376 1.33686 11.8376C1.50956 11.8376 1.68227 11.7716 1.81411 11.6398L6.49999 6.95436L11.1859 11.6398C11.3177 11.7716 11.4904 11.8376 11.6631 11.8376C11.8358 11.8376 12.0085 11.7716 12.1404 11.6398C12.404 11.3761 12.404 10.949 12.1404 10.6853L7.45455 5.99986Z" fill="#292929"/>
     </svg>`;
@@ -158,7 +164,7 @@ function mWebBuildElements() {
 
 function mWebCloseEvents() {
   const closeElements = document.querySelectorAll('.mweb-mobile-fork a[title="Continue"], .mweb-mobile-fork a.mweb-close');
-  closeElements.forEach(element => {
+  closeElements.forEach((element) => {
     element.addEventListener('click', (event) => {
       event.preventDefault();
       mWebOverlayScroll();
@@ -168,7 +174,7 @@ function mWebCloseEvents() {
 }
 
 function mWebVariant() {
-  if(!getMetadata('cta-1-icon') && !getMetadata('cta-1-link')) return;
+  if (!getMetadata('cta-1-icon') && !getMetadata('cta-1-link')) return;
   mWebBuildElements();
   mWebCloseEvents();
   mWebOverlayScroll();
@@ -201,4 +207,3 @@ export default async function decorate(block) {
 
   mWebVariant();
 }
-
