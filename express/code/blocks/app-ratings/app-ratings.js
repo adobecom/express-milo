9260ce9cdfb7dc2df8f31791a9c565cef36d1034
echen-adobe <all42238@adobe.com>
2025-04-03 09:07:24 -0700

    Update app-ratings.js

--- express/code/blocks/app-ratings/app-ratings.js
+++ express/code/blocks/app-ratings/app-ratings.js
@@ -6,20 +6,8 @@ let getConfig;
 const APPLE = 'apple';
 const GOOGLE = 'google';
 
-async function makeRating(store) {
-  const { replaceKey } = await import(`${getLibs()}/features/placeholders.js`);
+async function makeRating(store, ratingPlaceholder,starsPlaceholder,playStoreLabelPlaceholder,appleStoreLabelPlaceholder) {
 
-  const [ratingPlaceholder,
-    starsPlaceholder,
-    playStorePlaceholder,
-    appleStorePlaeholder] = await Promise.all(
-    [
-      await replaceKey('app-store-ratings', getConfig()),
-      await replaceKey('app-store-stars', getConfig()),
-      await replaceKey('app-store-ratings-play-store', getConfig()),
-      await replaceKey('app-store-ratings-apple-store', getConfig()),
-    ],
-  );
   const ratings = ratingPlaceholder?.split(';') || [];
   const link = ratings[2]?.trim();
   if (!link) {
@@ -28,8 +16,9 @@ async function makeRating(store) {
 
   const storeTypeIndex = [APPLE, GOOGLE].indexOf(store);
   const [score, cnt] = ratings[storeTypeIndex].split(',').map((str) => str.trim());
-  const ariaLabel = store === APPLE ? appleStorePlaeholder : playStorePlaceholder;
-  const storeLink = createTag('a', { href: link,
+  const ariaLabel = store === APPLE ? appleStoreLabelPlaceholder : playStoreLabelPlaceholder;
+  const storeLink = createTag('a', {
+    href: link,
   }, getIconElementDeprecated(`${store}-store`));
   storeLink.setAttribute('aria-label', ariaLabel);
   const { default: trackBranchParameters } = await import('../../scripts/branchlinks.js');
@@ -41,15 +30,15 @@ async function makeRating(store) {
   return createTag('div', { class: 'ratings-container' }, [score, star, cnt, storeLink]);
 }
 
-async function makeRatings() {
+async function makeRatings(ratingPlaceholder,starsPlaceholder,playStoreLabelPlaceholder,appleStoreLabelPlaceholder) {
   const ratings = createTag('div', { class: 'ratings' });
   const userAgent = getMobileOperatingSystem();
   if (userAgent !== 'Android') {
-    const appleElement = await makeRating('apple');
+    const appleElement = await makeRating('apple',ratingPlaceholder,starsPlaceholder,playStoreLabelPlaceholder,appleStoreLabelPlaceholder);
     appleElement && ratings.append(appleElement);
   }
   if (userAgent !== 'iOS') {
-    const googleElement = await makeRating('google');
+    const googleElement = await makeRating('google',ratingPlaceholder,starsPlaceholder,playStoreLabelPlaceholder,appleStoreLabelPlaceholder);
     googleElement && ratings.append(googleElement);
   }
   return ratings;
@@ -57,5 +46,17 @@ async function makeRatings() {
 
 export default async function decorate(block) {
   ({ createTag, getConfig } = await import(`${getLibs()}/utils/utils.js`));
-  block.append(await makeRatings());
+  const { replaceKey } = await import(`${getLibs()}/features/placeholders.js`);
+  const [ratingPlaceholder,
+    starsPlaceholder,
+    playStoreLabelPlaceholder,
+    appleStoreLabelPlaceholder] = await Promise.all(
+      [
+        replaceKey('app-store-ratings', getConfig()),
+        replaceKey('app-store-stars', getConfig()),
+        replaceKey('app-store-ratings-play-store', getConfig()),
+        replaceKey('app-store-ratings-apple-store', getConfig()),
+      ],
+    );
+  block.append(await makeRatings(ratingPlaceholder,starsPlaceholder,playStoreLabelPlaceholder,appleStoreLabelPlaceholder));
 }
