b94b6483d41c16c363386012ada66600f92b5ed9
Jackson Sandland <all48775@adobe.com>
2025-04-09 14:13:04 -0700

    ensure mobile cta only requires one click, remove Try it hover state button on desktop

--- express/code/blocks/cta-carousel/cta-carousel.js
+++ express/code/blocks/cta-carousel/cta-carousel.js
@@ -197,7 +197,9 @@ async function decorateCards(block, payload) {
           a.removeAttribute('title');
           a.setAttribute('aria-label', `${cta.text.toLowerCase().trim()} ${a.text.toLowerCase().trim()}`);
         }
-        linksWrapper.append(a);
+        const cardLink = createTag('a', { href: a.href, class: 'card-link' });
+        cardLink.append(cardSleeve, textWrapper);
+        card.append(cardLink);
       });
     } else {
       card.classList.add('coming-soon');
