ddab7b68863574b6470f860369f9011e1e8c809a
“JingleH” <jingleh321@gmail.com>
2025-04-28 10:35:11 -0500

    wip

--- express/code/blocks/template-x-query-config/taas-form.js
+++ express/code/blocks/template-x-query-config/taas-form.js
@@ -1,7 +1,68 @@
 /* eslint-disable class-methods-use-this */
 import { html, LitElement, css } from './lit.min.js';
 
+const supportedLanguages = [
+  'ar-SA',
+  'cs-CZ',
+  'da-DK',
+  'de-DE',
+  'es-ES',
+  'el-GR',
+  'en-US',
+  'fi-FI',
+  'fil-PH',
+  'fr-FR',
+  'hi-IN',
+  'id-ID',
+  'it-IT',
+  'i-DEFAULT',
+  'ja-JP',
+  'ko-KR',
+  'MS-MY',
+  'nb-NO',
+  'nl-NL',
+  'pl-PL',
+  'pt-BR',
+  'ro-RO',
+  'ru-RU',
+  'sv-SE',
+  'th-TH',
+  'tr-TR',
+  'uk-UA',
+  'vi-VN',
+  'zh-Hant-TW',
+  'zh-Hans-CN',
+];
+const supportedRegions = [];
+
 class TAASForm extends LitElement {
+  static properties = {
+    collectionId: { type: String },
+    limit: { type: Number },
+    start: { type: Number },
+    q: { type: String },
+    language: { type: String },
+    tasks: { type: String },
+    topics: { type: String },
+    license: { type: String },
+    prefLang: { type: String },
+    prefRegion: { type: String },
+  };
+
+  constructor() {
+    super();
+    this.collectionId = '';
+    this.limit = 70;
+    this.start = 0;
+    this.q = '';
+    this.language = '';
+    this.tasks = '';
+    this.topics = '';
+    this.license = '';
+    this.prefLang = '';
+    this.prefRegion = '';
+  }
+
   static styles = css`
     form {
       display: flex;
@@ -9,106 +70,176 @@ class TAASForm extends LitElement {
     }
   `;
 
-  handleGenerate(e) {
+  handleSubmit(e) {
     const formData = new FormData(e.target);
   }
 
   render() {
-    return html`<form>
-      <label>
-        Collection ID:
-        <input name="collectionID" type="text" />
-      </label>
-
-      <label>
-        Limit:
-        <input name="limit" type="number" aria-describedby="limit-desc" />
-      </label>
-      <p id="limit-desc">
-        <small
-          >Number of results to return. Leave empty to use the default limit
-          (e.g. 10).</small
-        >
-      </p>
-
-      <label>
-        Start:
-        <input name="start" type="number" />
-      </label>
-
-      <label>
-        Sort:
-        <select name="sort">
-          <option value="ascending">Ascending by Clicks</option>
-          <option value="descending">Descending by Clicks</option>
-          <option value="popular">Popular</option>
-        </select>
-      </label>
-
-      <label>
-        Q:
-        <input name="q" type="text" />
-      </label>
-
-      <label>
-        Language:
-        <input name="language" type="text" />
-      </label>
-
-      <label>
-        Tasks:
-        <input name="tasks" type="text" />
-      </label>
-
-      <label>
-        Topics:
-        <input name="topics" type="text" />
-      </label>
-
-      <fieldset>
-        <legend>Behaviors (max 2)</legend>
-        <p id="licensing-desc">
-          <small>
-            Optional field. Leave empty to include all licensing types.<br />
-            If selected, must be either <strong>Free</strong> or
-            <strong>Premium</strong>.
-          </small>
+    return html`<form @submit=${this.handleSubmit}>
+        <label>
+          Collection ID:
+          <input
+            name="collectionID"
+            type="text"
+            .value=${this.collectionId}
+            .input=${(e) => (this.collectionID = e.target.value)}
+          />
+        </label>
+        <p id="limit-desc">
+          <small
+            >Optional. Defaults to the global collection (urn:aaid:sc:VA6C2:25a82757-01de-4dd9-b0ee-bde51dd3b418). Another option is the
+            popular collection</small
+          >
         </p>
-        <label
-          ><input type="checkbox" name="behaviors" value="animated" />
-          Animated</label
-        >
-        <label
-          ><input type="checkbox" name="behaviors" value="video" /> Video</label
-        >
-        <label
-          ><input type="checkbox" name="behaviors" value="still" /> Still</label
-        >
-        <p id="behavior-error" style="color: red; display: none;">
-          Max 2 behaviors allowed.
+
+        <label>
+          Limit:
+          <input
+            name="limit"
+            type="number"
+            aria-describedby="limit-desc"
+            .value=${this.limit}
+            .input=${(e) => (this.limit = Number(e.target.value))}
+          />
+        </label>
+        <p id="limit-desc">
+          <small
+            >Number of results to return. Leave empty to use the default limit
+            (e.g. 10).</small
+          >
         </p>
-      </fieldset>
 
-      <label for="licensingCategory">Licensing Category:</label>
-      <select
-        name="licensingCategory"
-        id="licensingCategory"
-        aria-describedby="licensing-desc"
-      >
-        <option value="">Mixed (Free and Premium)</option>
-        <option value="free">Free</option>
-        <option value="premium">Premium</option>
-      </select>
-      <p id="licensing-desc">
-        <small>
-          Optional filter. Select <strong>Free</strong> or
-          <strong>Premium</strong> to narrow results.<br />
-          Leave as <strong>Mixed</strong> to include all licensing types.
-        </small>
-      </p>
+        <label>
+          Start:
+          <input
+            name="start"
+            type="number"
+            .value=${this.start}
+            .input=${(e) => (this.start = Number(e.target.value))}
+          />
+        </label>
 
-      <button type="submit">Generate</button>
-    </form>`;
+        <fieldset class="form-group">
+          <legend>Order by</legend>
+          <label
+            ><input type="radio" name="order-by" value="relevancy" />
+            Default by Relevancy</label
+          ><br />
+          <label
+            ><input type="radio" name="order-by" value="-remixCount" />
+            Descending by Remix Count</label
+          ><br />
+          <label
+            ><input type="radio" name="order-by" value="+remixCount" />
+            Ascending by Remix Count</label
+          ><br />
+          <label
+            ><input type="radio" name="order-by" value="-createDate" />
+            Descending by Create Date (Newest first)</label
+          >
+          <label
+            ><input type="radio" name="order-by" value="+createDate" />
+            Descending by Create Date (Oldest first)</label
+          >
+          <p><small>Select by which method results would be ordered.</small></p>
+        </fieldset>
+
+        <label>
+          Q:
+          <input name="q" type="text" />
+        </label>
+
+        <h2>Filters:</h2>
+        <label>
+          Language:
+          <input name="language" type="text" />
+        </label>
+
+        <label>
+          Tasks:
+          <input name="tasks" type="text" />
+        </label>
+
+        <label>
+          Topics:
+          <input name="topics" type="text" />
+        </label>
+
+        <fieldset class="form-group full-width">
+          <legend>Behaviors</legend>
+          <label
+            ><input type="radio" name="behaviors" value="" /> All
+            (Default)</label
+          ><br />
+          <label
+            ><input type="radio" name="behaviors" value="still" /> Still</label
+          ><br />
+          <label
+            ><input type="radio" name="behaviors" value="animated" />
+            Animated</label
+          ><br />
+          <label
+            ><input type="radio" name="behaviors" value="video" /> Video</label
+          ><br />
+          <label
+            ><input type="radio" name="behaviors" value="animated,video" />
+            Animated + Video</label
+          >
+          <p>
+            <small
+              >Select one behavior filter. Choose "All" to include all
+              types.</small
+            >
+          </p>
+        </fieldset>
+
+        <fieldset class="form-group full-width">
+          <legend>Licensing Category</legend>
+          <label
+            ><input type="radio" name="licensingCategory" value="" /> Mixed
+            (Free and Premium)</label
+          ><br />
+          <label
+            ><input type="radio" name="licensingCategory" value="free" />
+            Free</label
+          ><br />
+          <label
+            ><input type="radio" name="licensingCategory" value="premium" />
+            Premium</label
+          >
+          <p>
+            <small
+              >Select a category to filter results. Choose "Mixed" to include
+              both Free and Premium.</small
+            >
+          </p>
+        </fieldset>
+        <h2>Boosting:</h2>
+        <label>
+          Preferred Language Boosting:
+          <input name="pref-lang" />
+          <p><small>boost templates that are in this language</small></p>
+        </label>
+        <label>
+          Preferred Region Boosting:
+          <input name="pref-region" />
+          <p>
+            <small>boost templates that are in this country</small>
+          </p>
+        </label>
+
+        <button type="submit">Generate</button>
+      </form>
+      <div>For authors, copy this identifier:</div>
+      <div>
+        {collectionIdParam}{qParam}{limitParam}{startParam}{sortParam}{filterStr}
+      </div>
+      <div>
+        For developers debugging, this is the API call:
+        <div>
+          {base}?{collectionIdParam}{queryParam}{qParam}{limitParam}{startParam}{sortParam}{filterStr}
+        </div>
+      </div> `;
   }
 }
 
