7b56ac4173cb8286f11498fcb4b4af431f2a97a3
“JingleH” <jingleh321@gmail.com>
2025-05-05 16:34:05 -0700

    wip

--- express/code/blocks/template-x-query-config/taas-form.js
+++ express/code/blocks/template-x-query-config/taas-form.js
@@ -5,11 +5,14 @@
 import { html, LitElement, css } from './lit.min.js';
 import { getIconElementDeprecated } from '../../scripts/utils.js';
 
+const defaultCollectionId =
+  'urn:aaid:sc:VA6C2:25a82757-01de-4dd9-b0ee-bde51dd3b418';
 class TAASForm extends LitElement {
   static properties = {
     collectionId: { type: String },
     limit: { type: Number },
     start: { type: Number },
+    orderBy: { type: String },
     q: { type: String },
     language: { type: String },
     tasks: { type: String },
@@ -23,7 +26,7 @@ class TAASForm extends LitElement {
   constructor() {
     super();
     this.collectionId = '';
-    this.limit = 70;
+    this.limit = 10;
     this.start = 0;
     this.q = '';
     this.orderBy = '';
@@ -66,10 +69,49 @@ class TAASForm extends LitElement {
     }
   `;
 
-  handleSubmit(e) {
+  extractRecipe() {
+    const collectionId = `collectionId=${
+      this.collectionId || defaultCollectionId
+    }`;
+    const limit = this.limit ? `limit=${this.limit}` : '';
+    const start = this.start ? `start=${this.start}` : '';
+    const q = this.q ? `q=${this.q}` : '';
+    const language = this.language ? `language=${this.language}` : '';
+    const tasks = this.tasks ? `tasks=${this.tasks}` : '';
+    const topics = this.topics ? `topics=${this.topics}` : '';
+    const license = this.license ? `license=${this.license}` : '';
+    const behaviors = this.behaviors ? `behaviors=${this.behaviors}` : '';
+    const prefLang = this.prefLang ? `prefLang=${this.prefLang}` : '';
+    const prefRegion = this.prefRegion ? `prefRegion=${this.prefRegion}` : '';
+    const recipe = [
+      q,
+      topics,
+      tasks,
+      language,
+      license,
+      behaviors,
+      limit,
+      collectionId,
+      prefLang,
+      prefRegion,
+      start,
+    ]
+      .filter(Boolean)
+      .join('&');
+    console.log('extractRecipe', recipe);
+    return recipe;
+  }
+
+  handleGenerate(e) {
     e.preventDefault();
-    const formData = new FormData(e.target);
-    this.dispatchEvent(new CustomEvent('taas-form-submit', { detail: Object.fromEntries(formData), bubbles: true, composed: true }));
+    const recipe = this.extractRecipe();
+    this.dispatchEvent(
+      new CustomEvent('taas-generate-recipe', {
+        detail: recipe,
+        bubbles: true,
+        composed: true,
+      })
+    );
   }
 
   toggleInfoContent(fieldName) {
@@ -119,7 +161,10 @@ class TAASForm extends LitElement {
       'orderBy',
       'Select by which method results would be ordered'
     );
-    const [qInfoButton, qInfoContent] = this.getInfo('q', 'Search query. This is more flexible and ambiguous than using filters but also less precise.');
+    const [qInfoButton, qInfoContent] = this.getInfo(
+      'q',
+      'Search query. This is more flexible and ambiguous than using filters but also less precise.'
+    );
     const [languageInfoButton, languageInfoContent] = this.getInfo(
       'language',
       `Available values : AR_SA, BN_IN, CS_CZ, DA_DK, DE_DE, EL_GR, EN_US, ES_ES, FIL_PH, FI_FI, FR_FR, HI_IN, ID_ID, IT_IT, I_DEFAULT, JA_JP, KO_KR, MS_MY, NB_NO, NL_NL, PL_PL, PT_BR, RO_RO, RU_RU, SV_SE, TA_IN, TH_TH, TR_TR, UK_UA, VI_VN, ZH_HANS_CN, ZH_HANT_TW`
@@ -141,136 +186,163 @@ class TAASForm extends LitElement {
       `Available values : AD, AE, AF, AG, AI, AL, AM, AN, AO, AQ, AR, AS, AT, AU, AW, AX, AZ, BA, BB, BD, BE, BF, BG, BH, BI, BJ, BL, BM, BN, BO, BR, BS, BT, BV, BW, BY, BZ, CA, CC, CD, CF, CG, CH, CI, CK, CL, CM, CN, CO, CR, CU, CV, CX, CY, CZ, DE, DJ, DK, DM, DO, DZ, EC, EE, EG, EH, ER, ES, ET, FI, FJ, FK, FM, FO, FR, GA, GB, GD, GE, GF, GG, GH, GI, GL, GM, GN, GP, GQ, GR, GS, GT, GU, GW, GY, HK, HM, HN, HR, HT, HU, ID, IE, IL, IM, IN, IO, IQ, IR, IS, IT, JE, JM, JO, JP, KE, KG, KH, KI, KM, KN, KR, KV, KW, KY, KZ, LA, LB, LC, LI, LK, LR, LS, LT, LU, LV, LY, MA, MC, MD, ME, MF, MG, MH, MK, ML, MM, MN, MO, MP, MQ, MR, MS, MT, MU, MV, MW, MX, MY, MZ, NA, NC, NE, NF, NG, NI, NL, NO, NP, NR, NU, NZ, OM, PA, PE, PF, PG, PH, PK, PL, PM, PN, PR, PS, PT, PW, PY, QA, RE, RO, RS, RU, RW, SA, SB, SC, SD, SE, SG, SH, SI, SJ, SK, SL, SM, SN, SO, SR, ST, SV, SY, SZ, TC, TD, TF, TG, TH, TJ, TK, TL, TM, TN, TO, TR, TT, TV, TW, TZ, UA, UG, UM, US, UY, UZ, VA, VC, VE, VG, VI, VN, VU, WF, WS, YE, YT, ZA, ZM, ZW, ZZ`
     );
 
-    return html`<form @submit=${this.handleSubmit}>
-        <label>
-          Q:
-          <input name="q" type="text" />
-          ${qInfoButton}
-        </label>
-        ${qInfoContent}
+    return html`<form @submit=${this.handleGenerate}>
+      <label>
+        Q:
+        <input
+          name="q"
+          type="text"
+          .value=${this.q}
+          @input=${(e) => (this.q = e.target.value)}
+        />
+        ${qInfoButton}
+      </label>
+      ${qInfoContent}
 
-        <label>
-          Collection ID:
-          <input
-            name="collectionID"
-            type="text"
-            title="Optional. Defaults to the global collection (urn:aaid:sc:VA6C2:25a82757-01de-4dd9-b0ee-bde51dd3b418). Another common is the popular collection (urn:aaid:sc:VA6C2:a6767752-9c76-493e-a9e8-49b54b3b9852)."
-            .value=${this.collectionId}
-            .input=${(e) => (this.collectionID = e.target.value)}
-          />
-          ${collectionInfoButton}
-        </label>
-        ${collectionInfoContent}
+      <label>
+        Collection ID:
+        <input
+          name="collectionID"
+          type="text"
+          title="Optional. Defaults to the global collection (urn:aaid:sc:VA6C2:25a82757-01de-4dd9-b0ee-bde51dd3b418). Another common is the popular collection (urn:aaid:sc:VA6C2:a6767752-9c76-493e-a9e8-49b54b3b9852)."
+          .value=${this.collectionId}
+          @input=${(e) => (this.collectionID = e.target.value)}
+        />
+        ${collectionInfoButton}
+      </label>
+      ${collectionInfoContent}
 
-        <label>
-          Limit:
-          <input
-            name="limit"
-            type="number"
-            .value=${this.limit}
-            .input=${(e) => (this.limit = Number(e.target.value))}
-          />
-          ${limitInfoButton}
-        </label>
-        ${limitInfoContent}
+      <label>
+        Limit:
+        <input
+          name="limit"
+          type="number"
+          .value=${this.limit}
+          @input=${(e) => (this.limit = Number(e.target.value))}
+        />
+        ${limitInfoButton}
+      </label>
+      ${limitInfoContent}
 
-        <label>
-          Start:
-          <input
-            name="start"
-            type="number"
-            .value=${this.start}
-            .input=${(e) => (this.start = Number(e.target.value))}
-          />
-          ${startInfoButton}
-        </label>
-        ${startInfoContent}
+      <label>
+        Start:
+        <input
+          name="start"
+          type="number"
+          .value=${this.start}
+          @input=${(e) => (this.start = Number(e.target.value))}
+        />
+        ${startInfoButton}
+      </label>
+      ${startInfoContent}
 
-        <label>
-          Order by:
-          <select
-            name="order-by"
-            @change=${(e) => (this.orderBy = e.target.value)}
-          >
-            <option value="">Relevancy (Default)</option>
-            <option value="-remixCount">Descending by Remix Count</option>
-            <option value="+remixCount">Ascending by Remix Count</option>
-            <option value="-createDate">
-              Descending by Create Date (Newest first)
-            </option>
-            <option value="+createDate">
-              Ascending by Create Date (Oldest first)
-            </option>
-          </select>
-          ${orderByInfoButton}
-        </label>
-        ${orderByInfoContent}
+      <label>
+        Order by:
+        <select
+          name="order-by"
+          @change=${(e) => (this.orderBy = e.target.value)}
+        >
+          <option value="">Relevancy (Default)</option>
+          <option value="-remixCount">Descending by Remix Count</option>
+          <option value="+remixCount">Ascending by Remix Count</option>
+          <option value="-createDate">
+            Descending by Create Date (Newest first)
+          </option>
+          <option value="+createDate">
+            Ascending by Create Date (Oldest first)
+          </option>
+        </select>
+        ${orderByInfoButton}
+      </label>
+      ${orderByInfoContent}
 
-        <h2>Filters:</h2>
-        <label>
-          Language:
-          <input name="language" type="text" />
-          ${languageInfoButton}
-        </label>
-        ${languageInfoContent}
+      <h2>Filters (comma separated):</h2>
+      <label>
+        Language:
+        <input
+          name="language"
+          type="text"
+          .value=${this.language}
+          @input=${(e) => (this.language = e.target.value)}
+        />
+        ${languageInfoButton}
+      </label>
+      ${languageInfoContent}
 
-        <label>
-          Tasks:
-          <input name="tasks" type="text" />
-          ${tasksInfoButton}
-        </label>
-        ${tasksInfoContent}
+      <label>
+        Tasks:
+        <input
+          name="tasks"
+          type="text"
+          .value=${this.tasks}
+          @input=${(e) => (this.tasks = e.target.value)}
+        />
+        ${tasksInfoButton}
+      </label>
+      ${tasksInfoContent}
 
-        <label>
-          Topics:
-          <input name="topics" type="text" />
-          ${topicsInfoButton}
-        </label>
-        ${topicsInfoContent}
+      <label>
+        Topics:
+        <input
+          name="topics"
+          type="text"
+          .value=${this.topics}
+          @input=${(e) => (this.topics = e.target.value)}
+        />
+        ${topicsInfoButton}
+      </label>
+      ${topicsInfoContent}
 
-        <label>
-          Behaviors:
-          <select
-            name="behaviors"
-            @change=${(e) => (this.behaviors = e.target.value)}
-          >
-            <option value="">All (Default)</option>
-            <option value="still">Still</option>
-            <option value="animated">Animated</option>
-            <option value="video">Video</option>
-            <option value="animated+video">Animated + Video</option>
-          </select>
-        </label>
+      <label>
+        Behaviors:
+        <select
+          name="behaviors"
+          @change=${(e) => (this.behaviors = e.target.value)}
+        >
+          <option value="">All (Default)</option>
+          <option value="still">Still</option>
+          <option value="animated">Animated</option>
+          <option value="video">Video</option>
+          <option value="animated+video">Animated + Video</option>
+        </select>
+      </label>
 
-        <label>
-          Licensing Category:
-          <select
-            name="license"
-            @change=${(e) => (this.license = e.target.value)}
-          >
-            <option value="">Mixed (Default)</option>
-            <option value="free">Free only</option>
-            <option value="premium">Premium only</option>
-          </select>
-        </label>
+      <label>
+        Licensing Category:
+        <select
+          name="license"
+          @change=${(e) => (this.license = e.target.value)}
+        >
+          <option value="">Mixed (Default)</option>
+          <option value="free">Free only</option>
+          <option value="premium">Premium only</option>
+        </select>
+      </label>
 
-        <h2>Boosting:</h2>
-        <label>
-          Preferred Language Boosting:
-          <input name="pref-lang" />
-          ${prefLangInfoButton}
-        </label>
-        ${prefLangInfoContent}
-        <label>
-          Preferred Region Boosting:
-          <input name="pref-region" />
-          ${prefRegionInfoButton}
-        </label>
-        ${prefRegionInfoContent}
+      <h2>Boosting:</h2>
+      <label>
+        Preferred Language Boosting:
+        <input
+          name="pref-lang"
+          .value=${this.prefLang}
+          @input=${(e) => (this.prefLang = e.target.value)}
+        />
+        ${prefLangInfoButton}
+      </label>
+      ${prefLangInfoContent}
+      <label>
+        Preferred Region Boosting:
+        <input
+          name="pref-region"
+          .value=${this.prefRegion}
+          @input=${(e) => (this.prefRegion = e.target.value)}
+        />
+        ${prefRegionInfoButton}
+      </label>
+      ${prefRegionInfoContent}
 
-        <button type="submit">Generate</button>
-      </form>
-`;
+      <button type="submit">Generate</button>
+    </form> `;
   }
 }
 
