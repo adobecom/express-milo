414dca49dc460ec24e026f847852953983b40d59
“JingleH” <jingleh321@gmail.com>
2025-05-08 12:54:09 -0700

    added demo block

---
+++ express/code/blocks/ax-grid-demo/ax-grid-demo.js
@@ -0,0 +1,23 @@
+import { getLibs } from '../../scripts/utils.js';
+
+let createTag;
+
+export default async function init(block) {
+  ({ createTag } = await import(`${getLibs()}/utils/utils.js`));
+  // block.classList.add('ax-grid-container');
+  block.innerHTML = '';
+  block.append(
+    createTag('div', { class: 'ax-grid-container row' }, createTag('div', { class: 'ax-grid-col-1 bar' }, '1 col')),
+    createTag('div', { class: 'ax-grid-container row' }, createTag('div', { class: 'ax-grid-col-2 bar' }, '2 col')),
+    createTag('div', { class: 'ax-grid-container row' }, createTag('div', { class: 'ax-grid-col-3 bar' }, '3 col')),
+    createTag('div', { class: 'ax-grid-container row' }, createTag('div', { class: 'ax-grid-col-4 bar' }, '4 col')),
+    createTag('div', { class: 'ax-grid-container row' }, createTag('div', { class: 'ax-grid-col-5 bar' }, '5 col')),
+    createTag('div', { class: 'ax-grid-container row' }, createTag('div', { class: 'ax-grid-col-6 bar' }, '6 col')),
+    createTag('div', { class: 'ax-grid-container row' }, createTag('div', { class: 'ax-grid-col-7 bar' }, '7 col')),
+    createTag('div', { class: 'ax-grid-container row' }, createTag('div', { class: 'ax-grid-col-8 bar' }, '8 col')),
+    createTag('div', { class: 'ax-grid-container row' }, createTag('div', { class: 'ax-grid-col-9 bar' }, '9 col')),
+    createTag('div', { class: 'ax-grid-container row' }, createTag('div', { class: 'ax-grid-col-10 bar' }, '10 col')),
+    createTag('div', { class: 'ax-grid-container row' }, createTag('div', { class: 'ax-grid-col-11 bar' }, '11 col')),
+    createTag('div', { class: 'ax-grid-container row' }, createTag('div', { class: 'ax-grid-col-12 bar' }, '12 col')),
+  );
+}
