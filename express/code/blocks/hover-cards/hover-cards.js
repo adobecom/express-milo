09545b3243efbd3096391182f1d613dd237e4a78
“JingleH” <jingleh321@gmail.com>
2025-05-13 13:50:47 -0700

    wip

--- express/code/blocks/hover-cards/hover-cards.js
+++ express/code/blocks/hover-cards/hover-cards.js
@@ -4,4 +4,14 @@ let createTag;
 
 export default async function init(el) {
   ({ createTag } = await import(`${getLibs()}/utils/utils.js`));
+  const [headlineRow, ...cards] = el.children;
+  headlineRow.classList.add('headline-container');
+  const cardsContainer = createTag('div', { class: 'cards-container' }, cards);
+  el.append(cardsContainer);
+  cards.forEach((card) => {
+    card.classList.add('card');
+    const [media, cta] = card.children;
+    media.classList.add('media');
+    cta.classList.add('cta');
+  });
 }
