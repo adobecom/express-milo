62b288b75e48b0b23e63cbb004be9a950b9d4f31
“JingleH” <jingleh321@gmail.com>
2025-05-13 16:52:16 -0700

    done hover-cards

--- express/code/blocks/hover-cards/hover-cards.js
+++ express/code/blocks/hover-cards/hover-cards.js
@@ -1,17 +1,23 @@
 import { getLibs } from '../../scripts/utils.js';
 
-let createTag;
-
 export default async function init(el) {
-  ({ createTag } = await import(`${getLibs()}/utils/utils.js`));
-  const [headlineRow, ...cards] = el.children;
+  const { createTag } = await import(`${getLibs()}/utils/utils.js`);
+  const [headlineRow, hoverContainer, ...cards] = el.children;
+  const pic = hoverContainer.querySelector('picture');
+  const hoverImgSrc = pic?.querySelector('img')?.src;
+  hoverContainer.remove();
   headlineRow.classList.add('headline-container');
   const cardsContainer = createTag('div', { class: 'cards-container' }, cards);
-  el.append(cardsContainer);
+  const scrollContainer = createTag('div', { class: 'scroll-container' }, cardsContainer);
+  el.append(scrollContainer);
   cards.forEach((card) => {
     card.classList.add('card');
+    hoverImgSrc && card.style.setProperty('--hover-img', `url(${hoverImgSrc})`);
     const [media, cta] = card.children;
     media.classList.add('media');
     cta.classList.add('cta');
+    [...cta.querySelectorAll('a')].forEach((a) => {
+      a.classList.add('button', 'primary');
+    });
   });
 }
