382bd059a235a35535b6b3cdf6d36a1811d9c0da
echen-adobe <all42238@adobe.com>
2025-05-13 08:12:08 -0700

    removed unneeded comment

--- test/blocks/mobile-fork-button/mobile-fork-button.test.js
+++ test/blocks/mobile-fork-button/mobile-fork-button.test.js
@@ -2,9 +2,7 @@
 /* eslint-disable no-unused-vars */
 
 import { readFile } from '@web/test-runner-commands';
-import { expect } from '@esm-bundle/chai';
-// ... existing code ...
-
+import { expect } from '@esm-bundle/chai'; 
 import { buildAutoBlocks } from '../../../express/code/scripts/utils.js';
 
 const imports = await Promise.all([
