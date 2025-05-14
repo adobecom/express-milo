e3e808f5e86c441b68d5720f0b13d79fd772a495
Jackson Sandland <all48775@adobe.com>
2025-05-14 11:09:38 -0700

    adds aria-labels for text and img

--- express/code/blocks/highlight/highlight.js
+++ express/code/blocks/highlight/highlight.js
@@ -3,8 +3,24 @@ export default async function decorate(block) {
 
   rows.slice(0).forEach((row) => {
     row.classList.add('icon-row');
+
+    row.setAttribute('role', 'listitem');
+
     const textDiv = row.children[1];
-    if (textDiv) textDiv.classList.add('text');
+    if (textDiv) {
+      textDiv.classList.add('text');
+      const textContent = textDiv.textContent.trim();
+
+      row.setAttribute('aria-label', textContent);
+
+      const img = row.querySelector('img');
+      if (img) {
+        img.setAttribute('alt', textContent);
+      }
+    }
+
     block.appendChild(row);
   });
+
+  block.setAttribute('role', 'list');
 }
