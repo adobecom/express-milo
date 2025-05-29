83b4ffae390fc71243ac8fbcb9047e3292f49f1b
“JingleH” <jingleh321@gmail.com>
2025-05-29 14:18:43 -0500

    hover-cards hover effects

--- express/code/blocks/hover-cards/hover-cards.js
+++ express/code/blocks/hover-cards/hover-cards.js
@@ -5,13 +5,13 @@ export default async function init(el) {
   const { createTag } = await import(`${getLibs()}/utils/utils.js`);
   const [headlineRow, hoverContainer, ...cards] = el.children;
   const pic = hoverContainer.querySelector('picture');
-  const hoverImgSrc = pic?.querySelector('img')?.src;
   hoverContainer.remove();
   headlineRow.classList.add('headline-container');
   cards.forEach((card) => {
     card.classList.add('card');
-    hoverImgSrc && card.style.setProperty('--hover-img', `url(${hoverImgSrc})`);
-    const [media, cta] = card.children;
+    card.prepend(pic.cloneNode(true));
+    const [bgPic, media, cta] = card.children;
+    bgPic.classList.add('bg-pic');
     media.classList.add('media');
     cta.classList.add('cta');
     [...cta.querySelectorAll('a')].forEach((a) => {
