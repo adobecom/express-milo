d02619a7e9c0669afbe92898c53399b8d7fd0795
AdobeLinhart <dlinhart@adobe.com>
2025-05-30 12:49:38 -0600

    Initial checkin. Working state.

--- express/code/blocks/grid-marquee/grid-marquee.js
+++ express/code/blocks/grid-marquee/grid-marquee.js
@@ -1,4 +1,4 @@
-import { getLibs, yieldToMain, getMobileOperatingSystem, getIconElementDeprecated } from '../../scripts/utils.js';
+import { getLibs, yieldToMain, getMobileOperatingSystem, getIconElementDeprecated, getMetadata } from '../../scripts/utils.js';
 
 let createTag; let getConfig;
 
@@ -29,6 +29,16 @@ function drawerOn(drawer) {
   }
   currDrawer = drawer;
   if (!largeMQ.matches) document.body.classList.add('disable-scroll');
+}
+function mWebChevron(face) {
+  const metadata = getMetadata('mweb-card-chevron');
+  if (metadata === null) return;
+  const svg = `
+    <svg width="11" height="6" viewBox="0 0 11 6" fill="none" xmlns="http://www.w3.org/2000/svg" class="mweb-card-chevron">
+      <path fill-rule="evenodd" clip-rule="evenodd" d="M6.20703 5.70704C5.81641 6.09765 5.18358 6.09765 4.79296 5.70704L0.792965 1.70704C0.402345 1.31642 0.402345 0.683595 0.792965 0.292975C1.18358 -0.0976448 1.8164 -0.0976448 2.20702 0.292975L5.49999 3.58595L8.79296 0.292975C8.98827 0.0976648 9.24413 5.24521e-06 9.5 5.24521e-06C9.75586 5.24521e-06 10.0117 0.0976648 10.207 0.292975C10.5976 0.683595 10.5976 1.31642 10.207 1.70704L6.20703 5.70704Z" fill="#8F8F8F"/>
+    </svg>
+  `;
+  face.innerHTML += svg;
 }
 document.addEventListener('click', (e) => currDrawer && !currDrawer.closest('.card').contains(e.target) && drawerOff());
 let isTouch;
@@ -156,6 +166,7 @@ function toCard(drawer) {
   }, [face, drawer]);
 
   face.classList.add('face');
+  mWebChevron(face);
   addCardInteractions(card, drawer);
   const lazyCB = () => decorateDrawer(videoAnchor.href, face.querySelector('img').src, titleText, panels, panelsFrag, drawer);
   drawer.classList.add('drawer', 'hide');
--- express/code/blocks/mobile-fork-button/mobile-fork-button.js
+++ express/code/blocks/mobile-fork-button/mobile-fork-button.js
@@ -98,20 +98,107 @@ function collectFloatingButtonData() {
       mobileText: getMetadataLocal('mobile-floating-cta-text'),
       href: getMetadataLocal('main-cta-link'),
       text: getMetadataLocal('main-cta-text'),
+      forkStickyMobileHref: getMetadataLocal('cta-1-link'),
+      forkStickyMobileText: getMetadataLocal('cta-1-text'),
     },
     bubbleSheet: getMetadataLocal('floating-cta-bubble-sheet'),
     live: getMetadataLocal('floating-cta-live'),
     forkButtonHeader: getMetadataLocal('fork-button-header'),
   };
+
   for (let i = 1; i < 3; i += 1) {
-    const toolData = createToolData(metadataMap, i);
-    data.tools.push(toolData);
-    if (getTextWidth(toolData.anchor.textContent, 16) > LONG_TEXT_CUTOFF) {
-      data.longText = true;
+    const prefix = `fork-cta-${i}`;
+    const iconMetadata = getMetadataLocal(`${prefix}-icon`);
+    const iconTextMetadata = getMetadataLocal(`${prefix}-icon-text`);
+    const hrefMetadata = getMetadataLocal(`${prefix}-link`);
+    const textMetadata = getMetadataLocal(`${prefix}-text`);
+    if (!iconMetadata) break;
+    const completeSet = {
+      icon: getIconElementDeprecated(iconMetadata),
+      iconText: iconTextMetadata,
+      href: hrefMetadata,
+      text: textMetadata,
+    };
+
+    if (Object.values(completeSet).every((val) => !!val)) {
+      const {
+        href, text, icon, iconText,
+      } = completeSet;
+      const aTag = createTag('a', { title: text, href });
+      aTag.textContent = text;
+      if (getTextWidth(text, 16) > LONG_TEXT_CUTOFF) {
+        data.longText = true;
+      }
+      data.tools.push({
+        icon,
+        anchor: aTag,
+        iconText,
+      });
     }
   }
 
   return data;
+}
+
+async function mWebStickyCTA() {
+  const newBlock = createTag(
+    'div',
+    { class: 'floating-button-wrapper' },
+    '<div class="floating-button meta-powered"><div>mobile</div></div>',
+  );
+  const oldBlock = document.querySelector('.mobile-fork-button');
+  oldBlock.replaceWith(newBlock);
+
+  const audience = 'mobile';
+  const data = collectFloatingButtonData();
+
+  data.mainCta.text = data.mainCta.forkStickyMobileText || data.mainCta.text;
+  data.mainCta.href = data.mainCta.forkStickyMobileHref || data.mainCta.href;
+  await createFloatingButton(oldBlock, audience, data);
+}
+
+function mWebOverlayScroll() {
+  // This is to make scroll work when switching between mobile and desktop.
+  // May need rewrite for RTP.
+  const mobileForkButton = document.querySelector('.mobile-fork-button.mweb-mobile-fork');
+  if (mobileForkButton
+    && window.getComputedStyle(mobileForkButton, null).display !== 'none') {
+    document.body.style.overflow = 'hidden';
+  } else {
+    document.body.style.overflow = '';
+  }
+}
+
+function mWebBuildElements() {
+  const mobileForkButton = document.querySelector('.mobile-fork-button');
+  mobileForkButton.classList.add('mweb-mobile-fork');
+  const svg = `
+    <svg width="13" height="12" viewBox="0 0 13 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M7.45455 5.99986L12.1404 1.31442C12.4041 1.05075 12.4041 0.623598 12.1404 0.359925C11.8767 0.0962522 11.4496 0.0962522 11.1859 0.359925L6.5 5.04537L1.81412 0.359925C1.55044 0.0962522 1.12329 0.0962522 0.85962 0.359925C0.595947 0.623598 0.595947 1.05075 0.85962 1.31442L5.54545 5.99986L0.85962 10.6853C0.595947 10.949 0.595947 11.3761 0.85962 11.6398C0.991452 11.7716 1.16416 11.8376 1.33686 11.8376C1.50956 11.8376 1.68227 11.7716 1.81411 11.6398L6.49999 6.95436L11.1859 11.6398C11.3177 11.7716 11.4904 11.8376 11.6631 11.8376C11.8358 11.8376 12.0085 11.7716 12.1404 11.6398C12.404 11.3761 12.404 10.949 12.1404 10.6853L7.45455 5.99986Z" fill="#292929"/>
+    </svg>`;
+  const closeElement = createTag('a', { class: 'mweb-close' }, svg);
+  mobileForkButton.querySelector('div:first-child').prepend(closeElement);
+}
+
+function mWebCloseEvents() {
+  const closeElements = document.querySelectorAll('.mweb-mobile-fork a[title="Continue"], .mweb-mobile-fork a.mweb-close');
+  closeElements.forEach((element) => {
+    element.addEventListener('click', (event) => {
+      event.preventDefault();
+     
+      mWebStickyCTA();
+      mWebOverlayScroll();
+    });
+  });
+}
+
+function mWebVariant() {
+  const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  const hasCta1 = getMetadata('cta-1-icon') || getMetadata('cta-1-link');
+  if (!hasCta1 || !isMobileDevice) return;
+  mWebBuildElements();
+  mWebCloseEvents();
+  mWebOverlayScroll();
 }
 
 export default async function decorate(block) {
@@ -136,4 +223,6 @@ export default async function decorate(block) {
     document.dispatchEvent(linksPopulated);
   }
   if (data.longText) blockWrapper.classList.add('long-text');
+
+  mWebVariant();
 }
