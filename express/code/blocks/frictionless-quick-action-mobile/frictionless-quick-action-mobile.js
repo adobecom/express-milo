27d6c6cd9c5e165bdbc5c5c26ac441a7ed55c1c8
Revuru Kalyan Kumar <rkalyankumar@adobe.com>
2025-06-16 14:55:27 +0530

    remove videoDocConfig for animate from audio

--- express/code/blocks/frictionless-quick-action-mobile/frictionless-quick-action-mobile.js
+++ express/code/blocks/frictionless-quick-action-mobile/frictionless-quick-action-mobile.js
@@ -247,21 +247,13 @@ export async function runQuickAction(quickAction, data, block) {
       ccEverywhere.quickAction.convertToMP4(videoDocConfig, appConfig, exportConfig, contConfig);
       break;
     case 'animate-from-audio':
-      ccEverywhere.quickAction.animateFromAudio(
-        videoDocConfig,
-        appConfig,
-        exportConfig,
-        contConfig,
-      );
+      ccEverywhere.quickAction.animateFromAudio({}, appConfig, exportConfig, contConfig);
       break;
     case 'caption-video':
       ccEverywhere.quickAction.captionVideo(videoDocConfig, appConfig, exportConfig, contConfig);
       break;
     default: break;
   }
-
-  // scroll to the top of quick action container
-  quickActionContainer.scrollIntoView({ behavior: 'smooth' });
 }
 
 // eslint-disable-next-line default-param-last
