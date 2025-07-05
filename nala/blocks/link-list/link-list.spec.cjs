6fe47fb2f71601110a9916ce0e4a96cb462a86d5
Haris Bijli <all46558@adobe.com>
2025-07-04 19:11:23 -0700

    link-list automation

---
+++ nala/blocks/link-list/link-list.spec.cjs
@@ -0,0 +1,47 @@
+module.exports = {
+  name: 'Express link-list block',
+  features: [
+    {
+      tcid: '0',
+      name: '@link-list centered',
+      path: '/drafts/nala/blocks/link-list/link-list-centered',
+      data: {
+        h3Text: 'Explore by category',
+        button1Text: 'Convert to MP4',
+        button2Text: 'Convert to GIF',
+      },
+      tags: '@link-list @link-list-centered @express @smoke @regression @t1',
+    },
+    {
+      tcid: '1',
+      name: '@link-list large',
+      path: '/drafts/nala/blocks/link-list/link-list-large',
+      data: {
+        button1Text: 'Convert to MP4',
+        button2Text: 'Convert to GIF',
+      },
+      tags: '@link-list @link-list-large @express @smoke @regression @t2',
+    },
+    {
+      tcid: '2',
+      name: '@link-list shaded',
+      path: '/drafts/nala/blocks/link-list/link-list-shaded',
+      data: {
+        button1Text: 'Convert to MP4',
+        button2Text: 'Convert to GIF',
+      },
+      tags: '@link-list @link-list-shaded @express @smoke @regression @t3',
+    },
+        {
+      tcid: '3',
+      name: '@link-list leftalign',
+      path: '/drafts/nala/blocks/link-list/link-list-leftalign',
+      data: {
+        h3Text: 'Explore by collection',
+        button1Text: 'Community',
+        button2Text: 'Brands',
+      },
+      tags: '@link-list @link-list-leftalign @express @smoke @regression @t4',
+    },
+  ],
+};
