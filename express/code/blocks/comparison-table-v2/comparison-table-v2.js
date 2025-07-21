4f530947fc9c338e50b8932ccb714ab4b68de55a
echen-adobe <all42238@adobe.com>
2025-07-21 09:51:49 -0700

    Squashed commit of the following:
    commit 45982e9cc1c66cf5ffb8e42d0b4f4cfb05ecc277
    Author: echen-adobe <all42238@adobe.com>
    Date:   Mon Jul 21 09:51:39 2025 -0700
    
        Update comparison-table-v2.js
    
    commit c3edffddec21196eb62108153af49670a1519261
    Author: echen-adobe <all42238@adobe.com>
    Date:   Mon Jul 21 09:28:16 2025 -0700
    
        ...

--- express/code/blocks/comparison-table-v2/comparison-table-v2.js
+++ express/code/blocks/comparison-table-v2/comparison-table-v2.js
@@ -1,4 +1,4 @@
-import { getLibs, getMobileOperatingSystem, fixIcons, getIconDeprecated, addTempWrapperDeprecated, decorateButtonsDeprecated } from '../../scripts/utils.js';
+import { decorateButtonsDeprecated, getLibs } from '../../scripts/utils.js';
 
 let createTag;
 
@@ -119,7 +119,7 @@ function createTableRow(featureRowDiv) {
         if (cellIndex === 0) {
             tableCell.classList.add('feature-cell-header')
         }
-        tableCell.setAttribute('data-plan-index', cellIndex)
+        tableCell.setAttribute('data-plan-index', cellIndex - 1)
         tableCell.classList.add('feature-cell');
         for (let i = 0; i < cellContent.classList.length; i++) {
             tableCell.classList.add(cellContent.classList[i]);
@@ -129,7 +129,6 @@ function createTableRow(featureRowDiv) {
         handleCellIcons(tableCell);
         tableRow.appendChild(tableCell);
     });
-
     return tableRow;
 }
 
@@ -189,7 +188,7 @@ function convertToTable(sectionGroup, columnHeaders) {
 
 
 
-function createPlanSelector(headers, planIndex ) {
+function createPlanSelector(headers, planIndex) {
     const selectWrapper = document.createElement('div');
     selectWrapper.classList.add('plan-selector-wrapper');
 
@@ -201,32 +200,32 @@ function createPlanSelector(headers, planIndex ) {
     planSelector.setAttribute('aria-haspopup', 'listbox');
     planSelector.setAttribute('aria-expanded', 'false');
     planSelector.setAttribute('data-plan-index', planIndex);
-  
+
     selectWrapper.appendChild(planSelector);
     planSelector.appendChild(createPlanDropdownChoices(headers))
     return selectWrapper;
 }
 
-function createPlanDropdownChoices( headers) {
+function createPlanDropdownChoices(headers) {
     const planSelectorChoices = document.createElement('div');
     planSelectorChoices.classList.add('plan-selector-choices', 'invisible-content');
     planSelectorChoices.setAttribute('role', 'listbox');
     planSelectorChoices.setAttribute('aria-label', 'Plan options');
 
- 
+
     for (let i = 0; i < headers.length; i++) {
         const option = document.createElement('div');
         option.classList.add('plan-selector-choice');
         option.value = i;
-        option.textContent = headers[i]; 
+        option.textContent = headers[i];
         option.setAttribute('data-plan-index', i)
         option.setAttribute('role', 'option');
-        option.setAttribute('aria-selected', 'false'); 
+        option.setAttribute('aria-selected', 'false');
         planSelectorChoices.appendChild(option);
     }
     return planSelectorChoices;
 }
- 
+
 function applyColumnShading(headerGroup, comparisonBlock) {
     const columnShadingConfig = Array.from(headerGroup[0].querySelectorAll('div')).map((d) => d.textContent.trim());
     const rows = comparisonBlock.querySelectorAll('div');
@@ -249,7 +248,7 @@ function createStickyHeader(headerGroup, comparisonBlock) {
 
     const headers = Array.from(headerCells).map(cell => cell.textContent.trim())
     headers.splice(0, 1)
-    console.log(headers)
+
 
     headerCells.forEach((headerCell, cellIndex) => {
         if (cellIndex === 0) {
@@ -261,7 +260,7 @@ function createStickyHeader(headerGroup, comparisonBlock) {
             if (cellIndex === headerCells.length - 1) {
                 headerCell.classList.add('last');
             }
- 
+
             const planSelector = createPlanSelector(headers, cellIndex - 1);
             const lenght = headerCell.children.length;
             for (let i = 0; i < lenght; i++) {
@@ -337,42 +336,24 @@ export default async function decorate(comparisonBlock) {
     await Promise.all([import(`${getLibs()}/utils/utils.js`), import(`${getLibs()}/features/placeholders.js`), decorateButtonsDeprecated(comparisonBlock)]).then(([utils, placeholders, buttons]) => { createTag = utils.createTag });
     const blockChildren = Array.from(comparisonBlock.children);
     const contentSections = partitionContentBySeparators(blockChildren);
-
-   
-
     applyColumnShading(contentSections[0], comparisonBlock);
     comparisonBlock.innerHTML = '';
     const { stickyHeaderElement, columnTitles } = createStickyHeader(contentSections[0], comparisonBlock);
-   
-
-
-
-
-
     comparisonBlock.appendChild(stickyHeaderElement);
     for (let sectionIndex = 1; sectionIndex < contentSections.length; sectionIndex++) {
         const sectionTable = convertToTable(contentSections[sectionIndex], columnTitles);
         comparisonBlock.appendChild(sectionTable);
     }
- 
-
- 
     const planSelectors = Array.from(stickyHeaderElement.querySelectorAll('.plan-selector'))
     const comparisonTableState = new ComparisonTableState()
-
     comparisonTableState.initializePlanSelectors(comparisonBlock, planSelectors)
-
     initStickyBehavior(stickyHeaderElement, comparisonBlock);
-
-
-
-
 }
 
 
-class ComparisonTableState {
+export class ComparisonTableState {
     constructor() {
-        
+
         this.visiblePlans = [0, 1];
         this.selectedPlans = new Map();
         this.planSelectors = []
@@ -383,57 +364,55 @@ class ComparisonTableState {
         this.planSelectors = planSelectors
         this.planSelectors.forEach((selector, index) => {
             const choiceWrapper = selector.querySelector('.plan-selector-choices')
-           
+
             const options = Array.from(choiceWrapper.children)
             options.forEach(option => {
                 option.addEventListener('click', (e) => {
                     e.preventDefault();
-                    e.stopPropagation(); 
+                    e.stopPropagation();
                     this.updateVisiblePlan(parseInt(selector.dataset.planIndex), parseInt(option.dataset.planIndex))
                     Array.from(document.querySelectorAll('.plan-selector-choices')).forEach(choices => {
                         choices.classList.add('invisible-content')
                     });
                 })
             })
-     
+
             selector.addEventListener('click', (e) => {
                 e.preventDefault();
                 e.stopPropagation();
                 this.openDropdown(selector)
             });
-            if (! this.visiblePlans.includes(index)) {
+            if (!this.visiblePlans.includes(index)) {
                 selector.closest('.plan-cell').classList.toggle('invisible-content', 1)
-                this.updateTableCells(index, index, true)
             } else {
-                selector.closest('.plan-cell').classList.toggle('order-' + index)
-                this.updateTableCells(index, index, 0)
-            
+                selector.closest('.plan-cell').classList.toggle('invisible-content', 0)
             }
 
-
-      
+            this.comparisonBlock.querySelectorAll('tr').forEach((row, rowIndex) => {
+                const cells = row.querySelectorAll('.feature-cell:not(.feature-cell-header)')
+                for (let i = 0; i < cells.length; i++) {
+                    cells[i].classList.toggle('invisible-content', !this.visiblePlans.includes(i))
+                }
+            })
         })
         this.updatePlanSelectorOptions()
     }
 
-    updateTableCells (index, visiblePlanIndex, intialValue = -1 ) {
+    updateTableCells(selectorIndex, newPlanIndex) {
         const tableRows = this.comparisonBlock.querySelectorAll('tr')
-        
-        tableRows.forEach((row, rowIndex) => {
-            
-            const cells = row.querySelectorAll('.feature-cell:not(.feature-cell-header)')
-        
+        tableRows.forEach((row) => {
+            const cells = Array.from(row.querySelectorAll('.feature-cell:not(.feature-cell-header)'))
             if (cells.length === 0) {
                 return
             }
-            console.log(index,visiblePlanIndex, intialValue)
-            if (intialValue !== -1) {
-                cells[index].classList.toggle('invisible-content', intialValue)
-                cells[index].classList.toggle('order-' + visiblePlanIndex, intialValue)
-            } else {
-                cells[index].classList.toggle('invisible-content')
-                cells[index].classList.toggle('order-' + visiblePlanIndex)
-            }
+            const oldCell = cells.filter(cell => cell.dataset.planIndex === selectorIndex.toString())[0]
+            const newCell = cells.filter(cell => cell.dataset.planIndex === newPlanIndex.toString())[0]
+            oldCell.classList.toggle('invisible-content', true)
+            newCell.classList.toggle('invisible-content', false)
+            const parent = oldCell.parentElement
+            parent.insertBefore(newCell, oldCell)
+            parent.appendChild(oldCell)
+
         })
     }
 
@@ -441,33 +420,26 @@ class ComparisonTableState {
         const dropdown = selector.querySelector('.plan-selector-choices')
         dropdown.classList.toggle('invisible-content')
     }
- 
+
     updateVisiblePlan(selectorIndex, newPlanIndex) {
         const visiblePlanIndex = this.visiblePlans.indexOf(selectorIndex)
-       
-       // console.log(  Array.from(this.comparisonBlock.querySelector('tr')))
 
-        this.planSelectors[selectorIndex].closest('.plan-cell').classList.toggle('invisible-content', true)
-        this.planSelectors[selectorIndex].closest('.plan-cell').classList.remove('order-' + visiblePlanIndex)
-        this.updateTableCells(selectorIndex, visiblePlanIndex)
+        const oldHeader = this.planSelectors[selectorIndex].closest('.plan-cell')
+        const newHeader = this.planSelectors[newPlanIndex].closest('.plan-cell')
+
+        oldHeader.classList.toggle('invisible-content')
+        newHeader.classList.toggle('invisible-content')
+
+        const parent = oldHeader.parentElement
+        parent.insertBefore(newHeader, oldHeader)
+        parent.appendChild(oldHeader)
+
         this.visiblePlans[visiblePlanIndex] = newPlanIndex;
-        this.planSelectors[newPlanIndex].closest('.plan-cell').classList.toggle('invisible-content', false)
-        this.planSelectors[newPlanIndex].closest('.plan-cell').classList.add('order-' + visiblePlanIndex)
-        this.updateTableCells(newPlanIndex, visiblePlanIndex)
         this.updatePlanSelectorOptions()
-        for (let i = 0; i < this.planSelectors.length; i++) {
-            if (! this.visiblePlans.includes(i)) {
-                this.planSelectors[i].querySelector('.plan-selector-choices').classList.add('invisible-content')
-            } else {
-                this.planSelectors[i].querySelector('.plan-selector-choices').classList.remove('invisible-content')
-            }
-        }
-
-        
+        this.updateTableCells(selectorIndex, newPlanIndex)
     }
 
     updatePlanSelectorOptions() {
-        console.log(this.visiblePlans)
         for (let i = 0; i < this.planSelectors.length; i++) {
             const currentPlanSelectorChildren = this.planSelectors[i].querySelector('.plan-selector-choices').children;
             for (let j = 0; j < currentPlanSelectorChildren.length; j++) {
