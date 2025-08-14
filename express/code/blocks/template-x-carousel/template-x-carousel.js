c9cca6a4f5edebc7f57385692b22f726f4887d91
Yeiber Cano <cano@Yeibers-MacBook-Pro-2.local>
2025-08-14 10:20:36 -0700

    merge template-x-carousel from jingle

---
+++ express/code/blocks/template-x-carousel/template-x-carousel.js
@@ -0,0 +1,63 @@
+import { getLibs } from '../../scripts/utils.js';
+import { fetchResults, isValidTemplate } from '../../scripts/template-utils.js';
+import renderTemplate from '../template-x/template-rendering.js';
+import buildGallery from '../../scripts/widgets/gallery/gallery.js';
+
+let createTag;
+let getConfig;
+
+async function createTemplates(recipe) {
+  const res = await fetchResults(recipe);
+  const templates = await Promise.all(
+    res.items
+      .filter((item) => isValidTemplate(item))
+      .map((item) => renderTemplate(item)),
+  );
+  templates.forEach((tplt) => tplt.classList.add('template'));
+  return templates;
+}
+
+async function createTemplatesContainer(recipe) {
+  const templatesContainer = createTag('div', { class: 'templates-container' });
+  const [templates] = await Promise.all([createTemplates(recipe)]);
+  templatesContainer.append(...templates);
+  const { control: initialControl } = await buildGallery(
+    [...templates],
+    templatesContainer,
+    { center: true },
+  );
+  return {
+    templatesContainer,
+    control: initialControl,
+  };
+}
+
+function decorateHeadingRow(headingRow) {
+  headingRow.classList.add('heading-row');
+  headingRow.querySelector('h1,h2,h3,h4,h5')?.classList.add('heading');
+  [...headingRow.querySelectorAll('p')].forEach((p) => p.classList.add('subcopy'));
+}
+
+export default async function init(el) {
+  ({ createTag, getConfig } = await import(`${getLibs()}/utils/utils.js`));
+  const [headingRow, recipeRow] = el.children;
+  decorateHeadingRow(headingRow);
+  const recipe = recipeRow.textContent.trim();
+  recipeRow.remove();
+
+  try {
+    // TODO: lazy load templates
+    const [{ templatesContainer, control: galleryControl }] = await Promise.all(
+      [createTemplatesContainer(recipe, el)],
+    );
+    el.append(templatesContainer);
+    el.append(galleryControl);
+  } catch (err) {
+    window.lana?.log(`Error in template-x-carousel-toolbar: ${err}`);
+    if (getConfig().env.name === 'prod') {
+      el.remove();
+    } else {
+      el.textContent = 'Error loading templates, please refresh the page or try again later.';
+    }
+  }
+}
