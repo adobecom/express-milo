02364e6d6faed2e51984a4e0bbd85ebac7c44180
Eric Chen <159481679+echen-adobe@users.noreply.github.com>
2025-09-08 10:26:00 -0700

    implemented simplified pricing cards v2 nala test

---
+++ nala/blocks/simplified-pricing-cards-v2/simplified-pricing-cards-v2.spec.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  name: 'Express simplified-pricing-cards-v2 block',
+  features: [
+    {
+      tcid: '0',
+      name: '@simplified-pricing-cards-v2 block',
+      path: ['/drafts/nala/blocks/simplified-pricing-cards-v2'],
+      tags: '@express @smoke @regression @simplified-pricing-cards-v2',
+    },
+  ],
+};
+
