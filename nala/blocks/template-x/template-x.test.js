45709eaa41914f5bc70be218c35afdf1eded4028
Max Nelson <maxn@adobe.com>
2025-09-09 13:42:12 -0700

    added basic test block for template-x. Asserts that search icon is visible

---
+++ nala/blocks/template-x/template-x.test.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-plusplus */
+import { expect, test } from '@playwright/test';
+import { features } from './template-x.spec.js';
+import TemplateX from './template-x.page.js';
+
+let templateX;
+
+test.describe('template-x Block Test Suite', () => {
+  // before each test block
+  test.beforeEach(async ({ page }) => {
+    templateX = new TemplateX(page);
+  });
+
+  test(`${features[0].name},${features[0].tags}`, async ({ page, baseURL }) => {
+    console.info(`${baseURL}${features[0].path}`);
+    await test.step('Go to block test page', async () => {
+      await page.goto(`${baseURL}${features[0].path}`);
+      await page.waitForLoadState('domcontentloaded');
+      await expect(page).toHaveURL(`${baseURL}${features[0].path}`);
+      await page.waitForTimeout(3000);
+    });
+
+    await test.step('Verify search icon is displayed ', async () => {
+      await page.waitForLoadState();
+      await templateX.searchBarWrapper.scrollIntoViewIfNeeded();
+      await expect(templateX.searchBarWrapper).toBeVisible();
+    });
+  });
+});
