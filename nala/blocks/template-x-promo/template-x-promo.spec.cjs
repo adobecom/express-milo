ba213d83dd2f5b13ee95242e7f739c2c2563cfea
Yeiber Cano <cano@Yeibers-MacBook-Pro-2.local>
2025-09-15 18:03:39 -0700

    Revert test paths to original draft structure

--- nala/blocks/template-x-promo/template-x-promo.spec.cjs
+++ nala/blocks/template-x-promo/template-x-promo.spec.cjs
@@ -4,19 +4,19 @@ module.exports = {
     {
       tcid: '0',
       name: '@template-x-promo basic functionality',
-      path: ['/express/'],
+      path: ['/drafts/nala/blocks/template-x-promo/template-x-promo-basic'],
       tags: '@express @smoke @regression @template-x-promo',
     },
     {
       tcid: '1',
       name: '@template-x-promo with recipe parameters',
-      path: ['/express/create'],
+      path: ['/drafts/nala/blocks/template-x-promo/template-x-promo-recipe'],
       tags: '@express @smoke @regression @template-x-promo',
     },
     {
       tcid: '2',
       name: '@template-x-promo real API integration',
-      path: ['/express/business'],
+      path: ['/drafts/nala/blocks/template-x-promo/template-x-promo-api'],
       tags: '@express @smoke @regression @template-x-promo',
     },
   ],
