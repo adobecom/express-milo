9db2866b870d7c02531e8280b7b64a419c25d9fd
Yeiber Cano <cano@Yeibers-MacBook-Pro-2.local>
2025-09-15 17:47:17 -0700

    Revert "Use existing test page with template-x-promo block"
    This reverts commit 1b5a7a98d22f823df89ddc5c2e16bf8a4c899bd2.

--- nala/blocks/template-x-promo/template-x-promo.spec.cjs
+++ nala/blocks/template-x-promo/template-x-promo.spec.cjs
@@ -4,19 +4,19 @@ module.exports = {
     {
       tcid: '0',
       name: '@template-x-promo basic functionality',
-      path: ['/drafts/yeiber/discover-redesign-taas'],
+      path: ['/drafts/nala/blocks/template-x-promo/template-x-promo-basic'],
       tags: '@express @smoke @regression @template-x-promo',
     },
     {
       tcid: '1',
       name: '@template-x-promo with recipe parameters',
-      path: ['/drafts/yeiber/discover-redesign-taas'],
+      path: ['/drafts/nala/blocks/template-x-promo/template-x-promo-recipe'],
       tags: '@express @smoke @regression @template-x-promo',
     },
     {
       tcid: '2',
       name: '@template-x-promo real API integration',
-      path: ['/drafts/yeiber/discover-redesign-taas'],
+      path: ['/drafts/nala/blocks/template-x-promo/template-x-promo-api'],
       tags: '@express @smoke @regression @template-x-promo',
     },
   ],
