f0989f4225a43db27d6faff357d80ccb842d8ac9
Yeiber Cano <cano@Yeibers-MacBook-Pro-2.local>
2025-09-18 15:32:59 -0700

    Fix ESLint errors - add missing newlines and disable console warning

--- express/code/blocks/blog-posts-v2/blog-posts-v2.js
+++ express/code/blocks/blog-posts-v2/blog-posts-v2.js
@@ -289,6 +289,8 @@ function getDateFormatter(newLanguage) {
 
 function addRightChevronToViewAll(blockElement) {
   const link = blockElement.parentElement.parentElement.querySelector('.content a');
+  if (!link) return;
+
   const rightChevronSVGHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="15" height="16" viewBox="0 0 15 16" fill="none">
       <path fill-rule="evenodd" clip-rule="evenodd" d="M5.46967 2.86029C5.76256 2.5674 6.23744 2.5674 6.53033 2.86029L11.0303 7.3603C11.3232 7.65319 11.3232 8.12806 11.0303 8.42095L6.53033 
       12.921C6.23744 13.2138 5.76256 13.2138 5.46967 12.921C5.17678 12.6281 5.17678 12.1532 5.46967 11.8603L9.43934 7.89062L5.46967 3.92096C5.17678 3.62806 5.17678 3.15319 5.46967 2.86029Z" fill="#292929"/>
@@ -366,7 +368,7 @@ export default async function decorate(block) {
   /* localize view all */
   const viewAll = await replaceKey('view-all', getConfig()) || 'view all';
   const viewAllLink = block?.parentElement?.querySelector('.content a');
-  if (viewAll) {
+  if (viewAll && viewAllLink) {
     viewAllLink.textContent = `${viewAll.charAt(0).toUpperCase()}${viewAll.slice(1)}`;
   }
 
