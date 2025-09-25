0c001709802b0f6d2ddca406377fd652f095c800
Haris Bijli <all46558@adobe.com>
2025-09-24 17:04:25 -0700

    frictionless-qa-video

--- nala/blocks/frictionless-qa-video/frictionless-qa-video.page.cjs
+++ nala/blocks/frictionless-qa-video/frictionless-qa-video.page.cjs
@@ -10,8 +10,14 @@ export default class FrictionlessQaVideo {
     // Frictionless type locators
     this.type = {
       video_to_gif: page.locator('.frictionless-quick-action[data-frictionlesstype=convert-to-gif]').nth(nth),
+      video_crop: page.locator('.frictionless-quick-action[data-frictionlesstype=crop-video]').nth(nth),
+      video_trim: page.locator('.frictionless-quick-action[data-frictionlesstype=trim-video]').nth(nth),
       video_resize: page.locator('.frictionless-quick-action[data-frictionlesstype=resize-video]').nth(nth),
+      video_merge: page.locator('.frictionless-quick-action[data-frictionlesstype=merge-videos]').nth(nth),
+      video_convert_to_mp4: page.locator('.frictionless-quick-action[data-frictionlesstype=convert-to-mp4]').nth(nth),
     };
+
+    this.uploadButton = page.getByRole('link', { name: 'Upload your video' });
 
     // Convert to gif type details
     this.convertToGifHeading = this.type.video_to_gif.locator('h1');
@@ -19,15 +25,61 @@ export default class FrictionlessQaVideo {
     this.convertToGifImage = this.type.video_to_gif.locator('picture > img').nth(0);
     this.convertToGifDropzone = this.type.video_to_gif.locator('.dropzone').nth(0);
     this.convertToGifDropzoneText = this.convertToGifDropzone.locator('p').nth(0);
-    this.convertToGifUploadButton = this.convertToGifDropzone.locator('a').nth(0);
     this.convertToGifTermsAndPolicy = this.convertToGifDropzone.locator('p').nth(2);
+    this.convertToGifiFrame = this.type.video_to_gif.locator('iframe').nth(0);
+
+    // Crop video type details
+    this.cropVideoHeading = this.type.video_crop.locator('h1');
+    this.cropVideoContent = this.type.video_crop.locator('p').nth(0);
+    this.cropVideoImage = this.type.video_crop.locator('picture > img').nth(0);
+    this.cropVideoDropzone = this.type.video_crop.locator('.dropzone').nth(0);
+    this.cropVideoTermsAndPolicy = this.cropVideoDropzone.locator('p').nth(2);
+    this.cropVideoiFrame = this.type.video_crop.locator('iframe').nth(0);
+
+    // Trim video type details
+    this.trimVideoHeading = this.type.video_trim.locator('h1');
+    this.trimVideoContent = this.type.video_trim.locator('p').nth(0);
+    this.trimVideoImage = this.type.video_trim.locator('picture > img').nth(0);
+    this.trimVideoDropzone = this.type.video_trim.locator('.dropzone').nth(0);
+    this.trimVideoTermsAndPolicy = this.trimVideoDropzone.locator('p').nth(2);
+    this.trimVideoiFrame = this.type.video_trim.locator('iframe').nth(0);
 
     // Resize video type details
     this.resizeVideoHeading = this.type.video_resize.locator('h1');
     this.resizeVideoContent = this.type.video_resize.locator('p').nth(0);
     this.resizeVideoImage = this.type.video_resize.locator('picture > img').nth(0);
     this.resizeVideoDropzone = this.type.video_resize.locator('.dropzone').nth(0);
-    this.resizeVideoUploadButton = this.resizeVideoDropzone.locator('a').nth(0);
     this.resizeVideoTermsAndPolicy = this.resizeVideoDropzone.locator('p').nth(2);
+    this.resizeVideoiFrame = this.type.video_resize.locator('iframe').nth(0);
+
+    // Merge video type details
+    this.mergeVideoHeading = this.type.video_merge.locator('h1');
+    this.mergeVideoContent = this.type.video_merge.locator('p').nth(0);
+    this.mergeVideoImage = this.type.video_merge.locator('picture > img').nth(0);
+    this.mergeVideoDropzone = this.type.video_merge.locator('.dropzone').nth(0);
+    this.mergeVideoTermsAndPolicy = this.mergeVideoDropzone.locator('p').nth(2);
+    this.mergeVideoiFrame = this.type.video_merge.locator('iframe').nth(0);
+
+    // Convert to MP4 video type details
+    this.convertToMp4Heading = this.type.video_convert_to_mp4.locator('h1');
+    this.convertToMp4Content = this.type.video_convert_to_mp4.locator('p').nth(0);
+    this.convertToMp4Image = this.type.video_convert_to_mp4.locator('picture > img').nth(0);
+    this.convertToMp4Dropzone = this.type.video_convert_to_mp4.locator('.dropzone').nth(0);
+    this.convertToMp4TermsAndPolicy = this.convertToMp4Dropzone.locator('p').nth(2);
+    this.convertToMp4iFrame = this.type.video_convert_to_mp4.locator('iframe').nth(0);
+  }
+
+  async uploadVideo(path) {
+    try {
+      const [fileChooser] = await Promise.all([
+        this.page.waitForEvent('filechooser', { timeout: 10000 }),
+        this.uploadButton.click(),
+      ]);
+      await fileChooser.setFiles(path);
+    } catch (error) {
+      console.error('Error during file upload:', error);
+      throw error;
+    }
+    await this.page.waitForLoadState('domcontentloaded');
   }
 }
--- nala/blocks/frictionless-qa-video/frictionless-qa-video.test.cjs
+++ nala/blocks/frictionless-qa-video/frictionless-qa-video.test.cjs
@@ -1,8 +1,11 @@
+import path from 'path';
 import { expect, test } from '@playwright/test';
 import WebUtil from '../../libs/webutil.cjs';
 import { features } from './frictionless-qa-video.spec.cjs';
 import FrictionlessQA from './frictionless-qa-video.page.cjs';
 import { runAccessibilityTest } from '../../libs/accessibility.cjs';
+
+const videoFilePath = path.resolve(__dirname, '../../assets/test-video.mp4');
 
 let webUtil;
 let frictionlessQA;
@@ -19,7 +22,6 @@ test.describe('Express Frictionless QA Video Block test suite', () => {
   test(`${features[0].name},${features[0].tags}`, async ({ page, baseURL }) => {
     const { data } = features[0];
     const testUrl = `${baseURL}${features[0].path}${miloLibs}`;
-    console.info(`[Test Page]: ${testUrl}`);
 
     await test.step('Go to frictionless-qa-video(convert to GIF) block test page', async () => {
       await page.goto(testUrl);
@@ -32,8 +34,43 @@ test.describe('Express Frictionless QA Video Block test suite', () => {
       await expect(frictionlessQA.convertToGifHeading).toContainText(data.h1Text);
       await expect(frictionlessQA.convertToGifContent).toContainText(data.p1Text);
       await expect(frictionlessQA.convertToGifDropzoneText).toContainText(data.dropZoneText);
-      await expect(frictionlessQA.convertToGifUploadButton).toContainText(data.buttonText);
       await expect(frictionlessQA.convertToGifTermsAndPolicy).toContainText(data.p2Text);
+      await expect(frictionlessQA.uploadButton).toBeVisible();
+    });
+
+    await test.step('Verify accessibility', async () => {
+      await runAccessibilityTest({ page, testScope: frictionlessQA.type.video_to_gif });
+    });
+
+    await test.step('Verify analytics attributes', async () => {
+      await expect(frictionlessQA.section).toHaveAttribute('daa-lh', await webUtil.getSectionDaalh(1));
+      await expect(frictionlessQA.frictionlessQaVideo).toHaveAttribute('daa-lh', await webUtil.getBlockDaalh('frictionless-quick-action', 1));
+    });
+
+    await test.step('Upload a sample MP4 file', async () => {
+      await frictionlessQA.uploadVideo(videoFilePath);
+      await expect(frictionlessQA.convertToGifiFrame).toBeVisible({ timeout: 30000 });
+    });
+  });
+
+  // Test 1 : Crop video
+  test(`${features[1].name},${features[1].tags}`, async ({ page, baseURL }) => {
+    const { data } = features[1];
+    const testUrl = `${baseURL}${features[1].path}${miloLibs}`;
+
+    await test.step('Go to frictionless-qa-video(crop video) block test page', async () => {
+      await page.goto(testUrl);
+      await page.waitForLoadState('domcontentloaded');
+      await expect(page).toHaveURL(testUrl);
+    });
+
+    await test.step('Verify crop video block content/specs', async () => {
+      await expect(frictionlessQA.type.video_crop).toBeVisible();
+      await expect(frictionlessQA.cropVideoHeading).toContainText(data.h1Text);
+      await expect(frictionlessQA.cropVideoContent).toContainText(data.p1Text);
+      await expect(frictionlessQA.cropVideoDropzone).toContainText(data.dropZoneText);
+      await expect(frictionlessQA.cropVideoTermsAndPolicy).toContainText(data.p2Text);
+      await expect(frictionlessQA.uploadButton).toBeVisible();
     });
 
     await test.step('Verify analytics attributes', async () => {
@@ -42,14 +79,54 @@ test.describe('Express Frictionless QA Video Block test suite', () => {
     });
 
     await test.step('Verify accessibility', async () => {
-      await runAccessibilityTest({ page, testScope: frictionlessQA.type.video_to_gif });
+      await runAccessibilityTest({ page, testScope: frictionlessQA.type.video_crop });
+    });
+
+    await test.step('Upload a sample MP4 file', async () => {
+      await frictionlessQA.uploadVideo(videoFilePath);
+      await expect(frictionlessQA.cropVideoiFrame).toBeVisible({ timeout: 30000 });
     });
   });
 
-  // Test 1 : Resize video
-  test(`${features[1].name},${features[1].tags}`, async ({ page, baseURL }) => {
-    const { data } = features[1];
-    const testUrl = `${baseURL}${features[1].path}${miloLibs}`;
+  // Test 2 : Trim video
+  test(`${features[2].name},${features[2].tags}`, async ({ page, baseURL }) => {
+    const { data } = features[2];
+    const testUrl = `${baseURL}${features[2].path}${miloLibs}`;
+
+    await test.step('Go to frictionless-qa-video(trim video) block test page', async () => {
+      await page.goto(testUrl);
+      await page.waitForLoadState('domcontentloaded');
+      await expect(page).toHaveURL(testUrl);
+    });
+
+    await test.step('Verify trim video block content/specs', async () => {
+      await expect(frictionlessQA.type.video_trim).toBeVisible();
+      await expect(frictionlessQA.trimVideoHeading).toContainText(data.h1Text);
+      await expect(frictionlessQA.trimVideoContent).toContainText(data.p1Text);
+      await expect(frictionlessQA.trimVideoDropzone).toContainText(data.dropZoneText);
+      await expect(frictionlessQA.trimVideoTermsAndPolicy).toContainText(data.p2Text);
+      await expect(frictionlessQA.uploadButton).toBeVisible();
+    });
+
+    await test.step('Verify analytics attributes', async () => {
+      await expect(frictionlessQA.section).toHaveAttribute('daa-lh', await webUtil.getSectionDaalh(1));
+      await expect(frictionlessQA.frictionlessQaVideo).toHaveAttribute('daa-lh', await webUtil.getBlockDaalh('frictionless-quick-action', 1));
+    });
+
+    await test.step('Verify accessibility', async () => {
+      await runAccessibilityTest({ page, testScope: frictionlessQA.type.video_trim });
+    });
+
+    await test.step('Upload a sample MP4 file', async () => {
+      await frictionlessQA.uploadVideo(videoFilePath);
+      await expect(frictionlessQA.trimVideoiFrame).toBeVisible({ timeout: 30000 });
+    });
+  });
+
+  // Test 3 : Resize video
+  test(`${features[3].name},${features[3].tags}`, async ({ page, baseURL }) => {
+    const { data } = features[3];
+    const testUrl = `${baseURL}${features[3].path}${miloLibs}`;
     console.info(`[Test Page]: ${testUrl}`);
 
     await test.step('Go to frictionless-qa-video(resize video) block test page', async () => {
@@ -63,8 +140,8 @@ test.describe('Express Frictionless QA Video Block test suite', () => {
       await expect(frictionlessQA.resizeVideoHeading).toContainText(data.h1Text);
       await expect(frictionlessQA.resizeVideoContent).toContainText(data.p1Text);
       await expect(frictionlessQA.resizeVideoDropzone).toContainText(data.dropZoneText);
-      await expect(frictionlessQA.resizeVideoUploadButton).toContainText(data.buttonText);
       await expect(frictionlessQA.resizeVideoTermsAndPolicy).toContainText(data.p2Text);
+      await expect(frictionlessQA.uploadButton).toBeVisible();
     });
 
     await test.step('Verify analytics attributes', async () => {
@@ -74,6 +151,83 @@ test.describe('Express Frictionless QA Video Block test suite', () => {
 
     await test.step('Verify accessibility', async () => {
       await runAccessibilityTest({ page, testScope: frictionlessQA.type.video_resize });
+    });
+
+    await test.step('Upload a sample MP4 file', async () => {
+      await frictionlessQA.uploadVideo(videoFilePath);
+      await expect(frictionlessQA.resizeVideoiFrame).toBeVisible({ timeout: 30000 });
+    });
+  });
+
+  // Test 4 : Merge video
+  test(`${features[4].name},${features[4].tags}`, async ({ page, baseURL }) => {
+    const { data } = features[4];
+    const testUrl = `${baseURL}${features[4].path}${miloLibs}`;
+    console.info(`[Test Page]: ${testUrl}`);
+
+    await test.step('Go to frictionless-qa-video(merge video) block test page', async () => {
+      await page.goto(testUrl);
+      await page.waitForLoadState('domcontentloaded');
+      await expect(page).toHaveURL(testUrl);
+    });
+
+    await test.step('Verify merge video block content/specs', async () => {
+      await expect(frictionlessQA.type.video_merge).toBeVisible();
+      await expect(frictionlessQA.mergeVideoHeading).toContainText(data.h1Text);
+      await expect(frictionlessQA.mergeVideoContent).toContainText(data.p1Text);
+      await expect(frictionlessQA.mergeVideoDropzone).toContainText(data.dropZoneText);
+      await expect(frictionlessQA.mergeVideoTermsAndPolicy).toContainText(data.p2Text);
+      await expect(frictionlessQA.uploadButton).toBeVisible();
+    });
+
+    await test.step('Verify analytics attributes', async () => {
+      await expect(frictionlessQA.section).toHaveAttribute('daa-lh', await webUtil.getSectionDaalh(1));
+      await expect(frictionlessQA.frictionlessQaVideo).toHaveAttribute('daa-lh', await webUtil.getBlockDaalh('frictionless-quick-action', 1));
+    });
+
+    await test.step('Verify accessibility', async () => {
+      await runAccessibilityTest({ page, testScope: frictionlessQA.type.video_merge });
+    });
+
+    await test.step('Upload a sample MP4 file', async () => {
+      await frictionlessQA.uploadVideo(videoFilePath);
+      await expect(frictionlessQA.mergeVideoiFrame).toBeVisible({ timeout: 30000 });
+    });
+  });
+
+  // Test 5: Convert to MP4 video
+  test(`${features[5].name},${features[5].tags}`, async ({ page, baseURL }) => {
+    const { data } = features[5];
+    const testUrl = `${baseURL}${features[5].path}${miloLibs}`;
+    console.info(`[Test Page]: ${testUrl}`);
+
+    await test.step('Go to frictionless-qa-video(merge video) block test page', async () => {
+      await page.goto(testUrl);
+      await page.waitForLoadState('domcontentloaded');
+      await expect(page).toHaveURL(testUrl);
+    });
+
+    await test.step('Verify convert to MP4 video block content/specs', async () => {
+      await expect(frictionlessQA.type.video_convert_to_mp4).toBeVisible();
+      await expect(frictionlessQA.convertToMp4Heading).toContainText(data.h1Text);
+      await expect(frictionlessQA.convertToMp4Content).toContainText(data.p1Text);
+      await expect(frictionlessQA.convertToMp4Dropzone).toContainText(data.dropZoneText);
+      await expect(frictionlessQA.convertToMp4TermsAndPolicy).toContainText(data.p2Text);
+      await expect(frictionlessQA.uploadButton).toBeVisible();
+    });
+
+    await test.step('Verify analytics attributes', async () => {
+      await expect(frictionlessQA.section).toHaveAttribute('daa-lh', await webUtil.getSectionDaalh(1));
+      await expect(frictionlessQA.frictionlessQaVideo).toHaveAttribute('daa-lh', await webUtil.getBlockDaalh('frictionless-quick-action', 1));
+    });
+
+    await test.step('Verify accessibility', async () => {
+      await runAccessibilityTest({ page, testScope: frictionlessQA.type.video_convert_to_mp4 });
+    });
+
+    await test.step('Upload a sample MP4 file', async () => {
+      await frictionlessQA.uploadVideo(videoFilePath);
+      await expect(frictionlessQA.convertToMp4iFrame).toBeVisible({ timeout: 30000 });
     });
   });
 });
