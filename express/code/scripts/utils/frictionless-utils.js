b5ff6a814565266df5909f320895cbb9a43369d4
cloakedj <kanwardhananjaysingh0545@gmail.com>
2025-09-30 19:37:51 +0530

    CCEX-234744 - Addressing upload UI reset bug for safari

--- express/code/scripts/utils/frictionless-utils.js
+++ express/code/scripts/utils/frictionless-utils.js
@@ -445,3 +445,7 @@ export async function initProgressBar(replaceKey, getConfig) {
   progressBar.setAttribute('progress', '2');
   return progressBar;
 }
+
+export function isSafari() {
+  return /Safari/.test(navigator.userAgent);
+}
