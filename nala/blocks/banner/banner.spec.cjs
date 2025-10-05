c65d2a7e240ce710a47e14507441362693975d5e
Haris Bijli <all46558@adobe.com>
2025-10-05 12:32:40 -0700

    banner commit

---
+++ nala/blocks/banner/banner.spec.cjs
@@ -0,0 +1,46 @@
+module.exports = {
+  name: 'Express banner block',
+  features: [
+    {
+      tcid: '0',
+      name: '@banner default',
+      path: '/drafts/nala/blocks/banner/banner-default',
+      data: {
+        headingText: 'Ready to create standout content?',
+        buttonText: 'Start for free',
+      },
+      tags: '@banner @banner-default @express @smoke @regression @t1',
+    },
+    {
+      tcid: '1',
+      name: '@banner light',
+      path: '/drafts/nala/blocks/banner/banner-light',
+      data: {
+        headingText: 'Feeding the soul.',
+        pText: 'Watch the Buzzrocks story come to life through good food and good company.',
+        buttonText: 'Start for free',
+      },
+      tags: '@banner @banner-light @express @regression @t2',
+    },
+    {
+      tcid: '2',
+      name: '@banner standout',
+      path: '/drafts/nala/blocks/banner/banner-standout',
+      data: {
+        headingText: 'Easily create posters with AI in Adobe Express.',
+        buttonText: 'Get started for free',
+      },
+      tags: '@banner @banner-standout @express @regression @t3',
+    },
+    {
+      tcid: '3',
+      name: '@banner cool',
+      path: '/drafts/nala/blocks/banner/banner-cool',
+      data: {
+        headingText: 'Content creation made easy.',
+        buttonText: 'Get Adobe Express Free',
+      },
+      tags: '@banner @banner-cool @express @smoke @regression @t2',
+    },
+  ],
+};
