60760e0dd99879136fc9ef04df20cf7c4345dc0b
Haris Bijli <all46558@adobe.com>
2025-10-06 15:54:20 -0700

    additional tests

--- nala/blocks/banner/banner.test.cjs
+++ nala/blocks/banner/banner.test.cjs
@@ -18,13 +18,13 @@ test.describe('Express Banner Block test suite', () => {
   // Test 0 : Banner default
   test(`[Test Id - ${features[0].tcid}] ${features[0].name},${features[0].tags}`, async ({ page, baseURL }) => {
     const { data } = features[0];
-    const testUrl = `${baseURL}${features[0].path}${miloLibs}`;
-    console.info(`[Test Page]: ${testUrl}`);
+    const testPage = `${baseURL}${features[0].path}${miloLibs}`;
+    console.info(`[Test Page]: ${testPage}`);
 
-    await test.step('Go to quotes block test page', async () => {
-      await page.goto(testUrl);
+    await test.step('Go to banner block test page', async () => {
+      await page.goto(testPage);
       await page.waitForLoadState('domcontentloaded');
-      await expect(page).toHaveURL(testUrl);
+      await expect(page).toHaveURL(testPage);
     });
 
     await test.step('Verify banner default block content/specs', async () => {
@@ -43,20 +43,20 @@ test.describe('Express Banner Block test suite', () => {
     });
     await test.step('Validate card button click', async () => {
       await banner.defaultBannerButton.click();
-      expect(page.url).not.toBe(testUrl);
+      expect(page.url).not.toBe(testPage);
     });
   });
 
   // Test 1 : Banner light
   test(`[Test Id - ${features[1].tcid}] ${features[1].name},${features[1].tags}`, async ({ page, baseURL }) => {
     const { data } = features[1];
-    const testUrl = `${baseURL}${features[1].path}${miloLibs}`;
-    console.info(`[Test Page]: ${testUrl}`);
+    const testPage = `${baseURL}${features[1].path}${miloLibs}`;
+    console.info(`[Test Page]: ${testPage}`);
 
     await test.step('Go to banner light block test page', async () => {
-      await page.goto(testUrl);
+      await page.goto(testPage);
       await page.waitForLoadState('domcontentloaded');
-      await expect(page).toHaveURL(testUrl);
+      await expect(page).toHaveURL(testPage);
     });
 
     await test.step('Verify banner light block content/specs', async () => {
@@ -77,20 +77,20 @@ test.describe('Express Banner Block test suite', () => {
 
     await test.step('Validate card button click', async () => {
       await banner.lightVariantButton.click();
-      expect(page.url).not.toBe(testUrl);
+      expect(page.url).not.toBe(testPage);
     });
   });
 
   // Test 2 : Banner standout
   test(`[Test Id - ${features[2].tcid}] ${features[2].name},${features[2].tags}`, async ({ page, baseURL }) => {
     const { data } = features[2];
-    const testUrl = `${baseURL}${features[2].path}${miloLibs}`;
-    console.info(`[Test Page]: ${testUrl}`);
+    const testPage = `${baseURL}${features[2].path}${miloLibs}`;
+    console.info(`[Test Page]: ${testPage}`);
 
     await test.step('Go to banner standout block test page', async () => {
-      await page.goto(testUrl);
+      await page.goto(testPage);
       await page.waitForLoadState('domcontentloaded');
-      await expect(page).toHaveURL(testUrl);
+      await expect(page).toHaveURL(testPage);
     });
 
     await test.step('Verify banner standout block content/specs', async () => {
@@ -110,20 +110,20 @@ test.describe('Express Banner Block test suite', () => {
 
     await test.step('Validate card button click', async () => {
       await banner.standoutVariantButton.click();
-      expect(page.url).not.toBe(testUrl);
+      expect(page.url).not.toBe(testPage);
     });
   });
 
   // Test 3 : Banner cool
   test(`[Test Id - ${features[3].tcid}] ${features[3].name},${features[3].tags}`, async ({ page, baseURL }) => {
     const { data } = features[3];
-    const testUrl = `${baseURL}${features[3].path}${miloLibs}`;
-    console.info(`[Test Page]: ${testUrl}`);
+    const testPage = `${baseURL}${features[3].path}${miloLibs}`;
+    console.info(`[Test Page]: ${testPage}`);
 
     await test.step('Go to banner cool block test page', async () => {
-      await page.goto(testUrl);
+      await page.goto(testPage);
       await page.waitForLoadState('domcontentloaded');
-      await expect(page).toHaveURL(testUrl);
+      await expect(page).toHaveURL(testPage);
     });
 
     await test.step('Verify banner cool block content/specs', async () => {
@@ -143,7 +143,83 @@ test.describe('Express Banner Block test suite', () => {
 
     await test.step('Validate card button click', async () => {
       await banner.coolVariantButton.click();
-      expect(page.url).not.toBe(testUrl);
+      expect(page.url).not.toBe(testPage);
+    });
+  });
+
+  // Test 4 : Banner default heading h3
+  test(`[Test Id - ${features[4].tcid}] ${features[4].name},${features[4].tags}`, async ({ page, baseURL }) => {
+    const { data } = features[4];
+    const testPage = `${baseURL}${features[4].path}${miloLibs}`;
+    console.info(`[Test Page]: ${testPage}`);
+
+    await test.step('Go to banner block test page', async () => {
+      await page.goto(testPage);
+      await page.waitForLoadState('domcontentloaded');
+      await expect(page).toHaveURL(testPage);
+    });
+
+    await test.step('Verify banner default block content/specs', async () => {
+      await expect(banner.banner).toBeVisible();
+      await expect(banner.defaultBannerHeading3).toContainText(data.headingText);
+      await expect(banner.defaultBannerButton).toContainText(data.buttonText);
+    });
+
+    await test.step('Verify analytics attributes', async () => {
+      await expect(banner.section).toHaveAttribute('daa-lh', await webUtil.getSectionDaalh(1));
+      await expect(banner.banner).toHaveAttribute('daa-lh', await webUtil.getBlockDaalh('banner', 1));
+    });
+
+    await test.step('Verify accessibility', async () => {
+      await runAccessibilityTest({ page, testScope: banner.banner });
+    });
+    await test.step('Validate card button click', async () => {
+      await banner.defaultBannerButton.click();
+      expect(page.url).not.toBe(testPage);
+    });
+  });
+
+  // Test 5 : Banner light multiple buttons
+  test.only(`[Test Id - ${features[5].tcid}] ${features[5].name},${features[5].tags}`, async ({ page, baseURL }) => {
+    const { data } = features[5];
+    const testPage = `${baseURL}${features[5].path}${miloLibs}`;
+    console.info(`[Test Page]: ${testPage}`);
+
+    await test.step('Go to banner block test page', async () => {
+      await page.goto(testPage);
+      await page.waitForLoadState('domcontentloaded');
+      await expect(page).toHaveURL(testPage);
+    });
+
+    await test.step('Verify light variant with multiple buttons block content/specs', async () => {
+      await expect(banner.variants.light).toBeVisible();
+      await expect(banner.lightVariantHeading).toContainText(data.headingText);
+      await expect(banner.lightVariantButton).toContainText(data.buttonText);
+      await expect(banner.lightVariantButton2).toContainText(data.buttonText);
+      await expect(banner.lightVariantButton3).toContainText(data.buttonText);
+    });
+
+    await test.step('Verify analytics attributes', async () => {
+      await expect(banner.section).toHaveAttribute('daa-lh', await webUtil.getSectionDaalh(1));
+      await expect(banner.variants.light).toHaveAttribute('daa-lh', await webUtil.getBlockDaalh('banner', 1));
+    });
+
+    await test.step('Verify accessibility', async () => {
+      await runAccessibilityTest({ page, testScope: banner.variants.light });
+    });
+
+    await test.step('Validate card button click', async () => {
+      await banner.lightVariantButton.click();
+      expect(page.url).not.toBe(testPage);
+    });
+
+    await test.step('Validate card hover', async () => {
+      await page.goto(testPage);
+      await page.waitForLoadState('domcontentloaded');
+      const backgroundColorBefore = await banner.lightVariantButton.evaluate((element) => window.getComputedStyle(element).backgroundColor);
+      await banner.lightVariantButton.hover();
+      const backgroundColorAfter = await banner.lightVariantButton.evaluate((element) => window.getComputedStyle(element).backgroundColor);
+      expect(backgroundColorBefore).not.toEqual(backgroundColorAfter);
     });
   });
 });
