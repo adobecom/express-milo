96b23df07a73d3edb53f1ab6f8fd85f43748f1bd
Max Nelson <maxn@adobe.com>
2025-10-06 17:00:33 -0700

    added calls to other product detail APIs and included data locally to workaround CORS errors.

--- express/code/blocks/pdp-x-test-2/fetchData/fetchProductDetails.js
+++ express/code/blocks/pdp-x-test-2/fetchData/fetchProductDetails.js
@@ -1,4 +1,4 @@
-export default async function fetchProductDetails(productId) {
+export async function fetchProductDetails(productId) {
   let productDetailsFetch;
   try {
     productDetailsFetch = await fetch(`https://www.zazzle.com/svc/partner/adobeexpress/v1/getproduct?productId=${productId}`);
@@ -8,4 +8,40 @@ export default async function fetchProductDetails(productId) {
   const productDetailsJSON = await productDetailsFetch.json();
   const productDetails = productDetailsJSON.data;
   return productDetails;
+}
+
+export async function fetchProductPrice(productId) {
+  let productPriceFetch;
+  try {
+    productPriceFetch = await fetch(`https://www.zazzle.com/svc/partner/adobeexpress/v1/getproductpricepricing?productId=${productId}`);
+  } catch (error) {
+    productPriceFetch = await fetch('/express/code/blocks/pdp-x-test-2/sample_data/getProductPrice.json');
+  }
+  const productPriceJSON = await productPriceFetch.json();
+  const productPrice = productPriceJSON.data;
+  return productPrice;
+}
+
+export async function fetchProductReviews(productId) {
+  let productPriceFetch;
+  try {
+    productPriceFetch = await fetch(`https://www.zazzle.com/svc/partner/adobeexpress/v1/getreviews?productId=${productId}`);
+  } catch (error) {
+    productPriceFetch = await fetch('/express/code/blocks/pdp-x-test-2/sample_data/getReviews.json');
+  }
+  const productPriceJSON = await productPriceFetch.json();
+  const productPrice = productPriceJSON.data;
+  return productPrice;
+}
+
+export async function fetchProductShippingEstimates(productId, zip, qty) {
+  let productShippingEstimatesFetch;
+  try {
+    productShippingEstimatesFetch = await fetch(`https://www.zazzle.com/svc/partner/adobeexpress/v1/getshippingestimates?productId=${productId}&zip=${zip}&qty=${qty}`);
+  } catch (error) {
+    productShippingEstimatesFetch = await fetch('/express/code/blocks/pdp-x-test-2/sample_data/getShippingEstimate.json');
+  }
+  const productShippingEstimatesJSON = await productShippingEstimatesFetch.json();
+  const productShippingEstimates = productShippingEstimatesJSON.data;
+  return productShippingEstimates;
 }
--- express/code/blocks/pdp-x-test-2/utilities/utility-functions.js
+++ express/code/blocks/pdp-x-test-2/utilities/utility-functions.js
@@ -2,10 +2,11 @@ export function extractProductId(block) {
   return block.children[0].children[1].textContent;
 }
 
-export function normalizeProductDetailObject(APIResponse) {
+export function normalizeProductDetailObject(productDetails, productPrice) {
   const normalizedProductDetails = {
-    heroImage: APIResponse.product.initialPrettyPreferredViewUrl,
-    productTitle: APIResponse.product.title,
+    heroImage: productDetails.product.initialPrettyPreferredViewUrl,
+    productTitle: productDetails.product.title,
+    price: productPrice.unitPrice,
     deliveryEstimateStringText: 'Order today and get it by',
     deliveryEstimateDate: 'Aug 20 - 26',
   };
