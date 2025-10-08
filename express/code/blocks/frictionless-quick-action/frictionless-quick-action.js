090d722dee5e009895216b90a338814be2b5afda
Shairil Kansal <shairilk@adobe.com>
2025-10-08 17:42:04 +0530

    fixing the linters

--- express/code/blocks/frictionless-quick-action/frictionless-quick-action.js
+++ express/code/blocks/frictionless-quick-action/frictionless-quick-action.js
@@ -552,9 +552,13 @@ export default async function decorate(block) {
   cta.addEventListener('click', (e) => e.preventDefault(), false);
   // Fetch the base url for editor entry from upload cta and save it for later use.
   frictionlessTargetBaseUrl = cta.href;
-  if (quickAction === FRICTIONLESS_UPLOAD_QUICK_ACTIONS.removeBackgroundVariant1 || quickAction === FRICTIONLESS_UPLOAD_QUICK_ACTIONS.removeBackgroundVariant2) {
-    const isStage = urlParams.get('hzenv') === 'stage';
-    frictionlessTargetBaseUrl = isStage ? 'https://stage.express.adobe.com/new' : 'https://express.adobe.com/new';
+  if (quickAction === FRICTIONLESS_UPLOAD_QUICK_ACTIONS.removeBackgroundVariant1
+    || quickAction === FRICTIONLESS_UPLOAD_QUICK_ACTIONS.removeBackgroundVariant2) {
+      const urlParams = new URLSearchParams(window.location.search);
+      const isStage = urlParams.get('hzenv') === 'stage';
+      frictionlessTargetBaseUrl = isStage
+        ? 'https://stage.express.adobe.com/new'
+        : 'https://express.adobe.com/new';
   }
 
   const dropzoneHint = dropzone.querySelector('p:first-child');
