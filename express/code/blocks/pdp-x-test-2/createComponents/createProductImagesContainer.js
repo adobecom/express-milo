99a452a05a7eb18b75b01413b0ff3e2ebcf39177
Max Nelson <maxn@adobe.com>
2025-10-16 08:21:59 -0700

    fixing up image carousel functionality

--- express/code/blocks/pdp-x-test-2/createComponents/createProductImagesContainer.js
+++ express/code/blocks/pdp-x-test-2/createComponents/createProductImagesContainer.js
@@ -7,7 +7,7 @@ export default async function createProductImagesContainer(productDetails) {
   ({ createTag } = await import(`${getLibs()}/utils/utils.js`));
   const productImagesContainer = createTag('div', { class: 'pdpx-product-images-container' });
   const productHeroImageContainer = createTag('div', { class: 'pdpx-product-hero-image-container' });
-  const productHeroImage = createTag('img', { class: 'pdpx-product-hero-image', id: 'pdpx-product-hero-image', 'data-image-type': 'front', src: productDetails.heroImage });
+  const productHeroImage = createTag('img', { class: 'pdpx-product-hero-image', id: 'pdpx-product-hero-image', 'data-image-type': 'Front', src: productDetails.heroImage });
   const imageThumbnailCarouselContainer = createTag('div', { class: 'pdpx-image-thumbnail-carousel-container' });
   const productThumbnails = productDetails.realviews.filter((realview) => realview.type === 'Product' && realview.canBePreferred);
   for (let i = 0; i < productThumbnails.length; i += 1) {
@@ -15,7 +15,7 @@ export default async function createProductImagesContainer(productDetails) {
     const imageURL = buildRealViewImageUrl(productThumbnails[i].realviewParams);
     const imageThumbnailCarouselItemImage = createTag('img', { class: 'pdpx-image-thumbnail-carousel-item-image', 'data-image-type': productThumbnails[i].title, src: imageURL });
     imageThumbnailCarouselItem.addEventListener('click', (element) => {
-      productHeroImage.src = imageURL;
+      productHeroImage.src = element.currentTarget.querySelector('img').src;
       productHeroImage.dataset.imageType = element.currentTarget.dataset.imageType;
       // productHeroImage.setAttribute('data-image-type', productThumbnails[i].title);
     });
