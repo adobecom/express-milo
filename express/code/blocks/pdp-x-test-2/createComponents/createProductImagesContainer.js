558cb3f2b8198a425074999cadb56c097a50cbba
Max Nelson <maxn@adobe.com>
2025-10-20 16:25:19 -0700

    updated image carousel to correctly update image thumbnails based on available renditions for the given product configuration

--- express/code/blocks/pdp-x-test-2/createComponents/createProductImagesContainer.js
+++ express/code/blocks/pdp-x-test-2/createComponents/createProductImagesContainer.js
@@ -1,23 +1,30 @@
 import { getLibs } from '../../../scripts/utils.js';
-import { buildRealViewImageUrl } from '../utilities/utility-functions.js';
+// import { buildRealViewImageUrl } from '../utilities/utility-functions.js';
 
 let createTag;
 
-export default async function createProductImagesContainer(productDetails) {
+export default async function createProductImagesContainer(realViews, heroImage, heroImageType = 'Front') {
   ({ createTag } = await import(`${getLibs()}/utils/utils.js`));
-  const productImagesContainer = createTag('div', { class: 'pdpx-product-images-container' });
+  const productImagesContainer = createTag('div', { class: 'pdpx-product-images-container', id: 'pdpx-product-images-container' });
   const productHeroImageContainer = createTag('div', { class: 'pdpx-product-hero-image-container' });
-  const productHeroImage = createTag('img', { class: 'pdpx-product-hero-image', id: 'pdpx-product-hero-image', 'data-image-type': 'Front', src: productDetails.heroImage });
+  const productHeroImage = createTag('img', { class: 'pdpx-product-hero-image', id: 'pdpx-product-hero-image', 'data-image-type': heroImageType, src: heroImage });
   const imageThumbnailCarouselContainer = createTag('div', { class: 'pdpx-image-thumbnail-carousel-container' });
-  const productThumbnails = productDetails.realviews.filter((realview) => realview.type === 'Product' && realview.canBePreferred);
-  for (let i = 0; i < productThumbnails.length; i += 1) {
-    const imageThumbnailCarouselItem = createTag('button', { class: 'pdpx-image-thumbnail-carousel-item', 'data-image-type': productThumbnails[i].title });
-    const imageURL = buildRealViewImageUrl(productThumbnails[i].realviewParams);
-    const imageThumbnailCarouselItemImage = createTag('img', { class: 'pdpx-image-thumbnail-carousel-item-image', 'data-image-type': productThumbnails[i].title, src: imageURL });
+  for (let i = 0; i < Object.keys(realViews).length; i += 1) {
+    const imageThumbnailCarouselItem = createTag('button', { class: 'pdpx-image-thumbnail-carousel-item', 'data-image-type': Object.keys(realViews)[i] });
+    if (heroImageType === Object.keys(realViews)[i]) {
+      imageThumbnailCarouselItem.classList.add('selected');
+    }
+    const imageURL = realViews[Object.keys(realViews)[i]];
+    const imageThumbnailCarouselItemImage = createTag('img', { class: 'pdpx-image-thumbnail-carousel-item-image', 'data-image-type': Object.keys(realViews)[i], src: imageURL });
     imageThumbnailCarouselItem.addEventListener('click', (element) => {
+      imageThumbnailCarouselItem.classList.add('selected');
+      imageThumbnailCarouselContainer.querySelectorAll('.pdpx-image-thumbnail-carousel-item').forEach((item) => {
+        if (item !== element.currentTarget) {
+          item.classList.remove('selected');
+        }
+      });
       productHeroImage.src = element.currentTarget.querySelector('img').src;
       productHeroImage.dataset.imageType = element.currentTarget.dataset.imageType;
-      // productHeroImage.setAttribute('data-image-type', productThumbnails[i].title);
     });
     imageThumbnailCarouselItem.appendChild(imageThumbnailCarouselItemImage);
     imageThumbnailCarouselContainer.appendChild(imageThumbnailCarouselItem);
