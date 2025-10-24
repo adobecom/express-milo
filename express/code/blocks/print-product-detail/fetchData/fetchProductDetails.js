d2d6376d927513bf518f2af267e0d012efa324ac
Max Nelson <maxn@adobe.com>
2025-10-23 17:00:26 -0700

    Revert "MWPW-182736 - PDP LCP"

--- express/code/blocks/print-product-detail/fetchData/fetchProductDetails.js
+++ express/code/blocks/print-product-detail/fetchData/fetchProductDetails.js
@@ -1,22 +1,30 @@
 export function formatUrlForEnvironment(url) {
   if (window.location.hostname === 'localhost') {
-    return `http://localhost:3001?url=${encodeURIComponent(url)}`;
+    return `http://localhost:3001?url=${url}`;
   }
   const URLFormatted = url;
   return URLFormatted;
 }
 
 export default async function fetchAPIData(productId, parameters, endpoint) {
+  let apiDataFetch;
+  let parametersString;
+  let url;
+  let topLevelDomain;
   const urlParams = new URLSearchParams(window.location.search);
   const region = urlParams.get('region');
-  const topLevelDomain = region === 'uk' ? 'co.uk' : 'com';
-  const parametersString = parameters ? Object.entries(parameters).map(([key, value]) => `${key}=${encodeURIComponent(value)}`).join('&') : '';
+  if (region === 'uk') {
+    topLevelDomain = 'co.uk';
+  } else {
+    topLevelDomain = 'com';
+  }
+  if (parameters) {
+    parametersString = Object.entries(parameters).map(([key, value]) => `${key}=${value}`).join('&');
+  } else {
+    parametersString = '';
+  }
 
-  const base = `https://www.zazzle.${topLevelDomain}/svc/partner/adobeexpress/v1/${endpoint}?productId=${productId}`;
-  const separator = parametersString ? '&' : '';
-  const url = `${base}${separator}${parametersString}`;
-
-  let apiDataFetch;
+  url = `https://www.zazzle.${topLevelDomain}/svc/partner/adobeexpress/v1/${endpoint}?productId=${productId}&${parametersString}`;
   try {
     apiDataFetch = await fetch(formatUrlForEnvironment(url));
   } catch (error) {
