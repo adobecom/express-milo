694581fe51a5bdf88d2248f90167bb1ae3358c3a
Yeiber Cano <yeibercano@gmail.com>
2025-10-28 11:04:39 -0700

    feat: add utility functions for deferred rendering (formatPriceZazzle, formatLargeNumberToK, addPrefetchLinks)

--- express/code/blocks/print-product-detail/utilities/utility-functions.js
+++ express/code/blocks/print-product-detail/utilities/utility-functions.js
@@ -1,20 +1,4 @@
-import fetchAPIData, { formatUrlForEnvironment } from '../fetchData/fetchProductDetails.js';
-
-export async function extractProductId(block) {
-  const templateId = block.children[0].children[1].textContent;
-
-  const url = `https://www.zazzle.com/svc/partner/adobeexpress/v1/getproductfromtemplate?templateID=${templateId}`;
-  const productIdAPICall = await fetch(formatUrlForEnvironment(url));
-  const productIdAPICallJSON = await productIdAPICall.json();
-
-  const productId = productIdAPICallJSON.data.product.id;
-
-  // FOR DEVELOPMENT PURPOSES ONLY, REMOVE IN PRODUCTION
-  const urlParams = new URLSearchParams(window.location.search);
-  const productIdURL = urlParams.get('productId');
-  const productIdFinal = productIdURL || productId;
-  return productIdFinal;
-}
+import { getLibs, createTag } from '../../../scripts/utils.js';
 
 export function formatDeliveryEstimateDateRange(minDate, maxDate) {
   const options = { month: 'short', day: 'numeric' };
@@ -45,20 +29,20 @@ export function formatLargeNumberToK(totalReviews) {
   return totalReviews;
 }
 
-export function formatPriceZazzle(price, differential = false, short = false) {
-  let countryCode;
-  let currencyCode;
-  let priceDifferentialOperator;
-  const urlParams = new URLSearchParams(window.location.search);
-  const region = urlParams.get('region');
-  if (region === 'uk') {
-    countryCode = 'en-GB';
-    currencyCode = 'GBP';
-  } else {
-    countryCode = 'en-US';
-    currencyCode = 'USD';
+export function exchangeRegionForTopLevelDomain(region) {
+  if (region === 'en-GB') {
+    return 'co.uk';
   }
-  const localizedPrice = new Intl.NumberFormat(countryCode, { style: 'currency', currency: currencyCode }).format(price);
+  return 'com';
+}
+
+export async function formatPriceZazzle(price, differential = false) {
+  const { getCountry } = await import('../../../scripts/utils/location-utils.js');
+  const country = await getCountry();
+  const { getCurrency, formatPrice } = await import('../../../scripts/utils/pricing.js');
+  const currency = await getCurrency(country);
+  let priceDifferentialOperator;
+  const localizedPrice = await formatPrice(price, currency);
   if (differential) {
     priceDifferentialOperator = price >= 0 ? '+' : '';
   } else {
@@ -72,4 +56,31 @@ export function formatStringSnakeCase(string) {
   const normalizedString = string.replace(/[^a-zA-Z0-9\s]/g, '_');
   const formattedString = normalizedString.trim().toLowerCase().replace(/ /g, '_');
   return formattedString;
+}
+
+export async function addPrefetchLinks() {
+  const { getConfig } = await import(`${getLibs()}/utils/utils.js`);
+  const { ietf } = getConfig().locale;
+  const topLevelDomain = exchangeRegionForTopLevelDomain(ietf);
+  const prefetchLink1 = createTag('link', {
+    rel: 'dns-prefetch',
+    href: `https://www.zazzle.${topLevelDomain}`,
+  });
+  const prefetchLink2 = createTag('link', {
+    rel: 'dns-prefetch',
+    href: `https://rlv.zcache.${topLevelDomain}`,
+  });
+
+  const preconnectLink1 = createTag('link', {
+    rel: 'preconnect',
+    href: `https://www.zazzle.${topLevelDomain}`,
+  });
+  const preconnectLink2 = createTag('link', {
+    rel: 'preconnect',
+    href: `https://www.zazzle.${topLevelDomain}`,
+  });
+  document.head.appendChild(prefetchLink1);
+  document.head.appendChild(prefetchLink2);
+  document.head.appendChild(preconnectLink1);
+  document.head.appendChild(preconnectLink2);
 }
