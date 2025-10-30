58b077d5ac895d4984faf5b6cef55db9c6ae3bcd
Nate Yolles <yolles@adobe.com>
2025-10-30 14:56:27 -0600

    blah2

--- express/code/blocks/discover-cards/discover-cards.js
+++ express/code/blocks/discover-cards/discover-cards.js
@@ -228,8 +228,6 @@ export default async function decorate(block) {
     class: 'background-image',
     src: '/express/code/blocks/discover-cards/img/cards-bg-large.webp',
     alt: '',
-    fetchpriority: 'high',
-    loading: 'eager',
   });
   block.appendChild(backgroundImg);
 }
